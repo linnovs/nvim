d9a6d70d0b969862d967acebfbe5630a544458b9
Linnovs Liu <7882261+linnovs@users.noreply.github.com>
2021-09-12 18:47:03 +0800

    feat(plugin): add backCauldro7/surround.nvim

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -9,7 +9,15 @@ return require("packer").startup(function(use)
 	-- better mapping handling
 	use({ "svermeulen/vimpeccable" })
 
+	-- commenting
+	use({ "b3nj5m1n/kommentary" })
 
+	use({
+		"blackCauldron7/surround.nvim",
+		config = function()
+			require("surround").setup({ mappings_style = "surround" })
+		end,
+	})
 
 
 	-- }}}
