3d92de13c7f50a9054b33168cac840ebb1a2b1cb
Linnovs Liu <7882261+linnovs@users.noreply.github.com>
2021-09-12 13:11:01 +0800

    feat: add plugin manager packer & vimp & window mgt mappings

--- lua/nebula/init.lua
+++ lua/nebula/init.lua
@@ -1,1 +1,2 @@
 require('nebula.options')
+require('nebula.mappings')
---
+++ lua/plugins.lua
@@ -0,0 +1,14 @@
+-- vim: foldmethod=marker
+
+return require('packer').startup(function()
+    -- Packer manage itself
+    use { 'wbthomason/packer.nvim' }
+
+    -- Utility {{{
+
+    -- better mapping handling
+    use { 'svermeulen/vimpeccable' }
+
+    -- }}}
+
+end)
