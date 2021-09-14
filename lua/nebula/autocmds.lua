67bea3969fbf347529944a2206bbbf45dbabb8f1
Linnovs Liu <7882261+linnovs@users.noreply.github.com>
2021-09-14 21:14:42 +0800

    feat: add my color definition

--- lua/nebula/autocmds.lua
+++ lua/nebula/autocmds.lua
@@ -1,16 +1,18 @@
+local colors = require("nebula.colors")
+local lsp = colors.lsp
 local autocmds = {
 	set_formatoptions = {
 		{ "BufEnter", "*", "setlocal formatoptions-=o" },
 	},
 	LspColors = {
-		{ "ColorScheme", "*", "highlight LspDiagnosticsDefaultError guifg=#db4b4b" },
-		{ "ColorScheme", "*", "highlight LspDiagnosticsDefaultWarning guifg=#e0af68" },
-		{ "ColorScheme", "*", "highlight LspDiagnosticsDefaultInformation guifg=#0db9d7" },
-		{ "ColorScheme", "*", "highlight LspDiagnosticsDefaultHint guifg=#10b981" },
-		{ "ColorScheme", "*", "highlight LspDiagnosticsUnderlineError gui=undercurl guisp=#db4b4b" },
-		{ "ColorScheme", "*", "highlight LspDiagnosticsUnderlineWarning gui=undercurl guisp=#e0af68" },
-		{ "ColorScheme", "*", "highlight LspDiagnosticsUnderlineInformation gui=undercurl guisp=#0db9d7" },
-		{ "ColorScheme", "*", "highlight LspDiagnosticsUnderlineHint gui=undercurl guisp=#10b981" },
+		{ "ColorScheme", "*", "highlight LspDiagnosticsDefaultError guifg=" .. lsp.error },
+		{ "ColorScheme", "*", "highlight LspDiagnosticsDefaultWarning guifg=" .. lsp.warning },
+		{ "ColorScheme", "*", "highlight LspDiagnosticsDefaultInformation guifg=" .. lsp.information },
+		{ "ColorScheme", "*", "highlight LspDiagnosticsDefaultHint guifg=" .. lsp.hint },
+		{ "ColorScheme", "*", "highlight LspDiagnosticsUnderlineError gui=undercurl guisp=" .. lsp.error },
+		{ "ColorScheme", "*", "highlight LspDiagnosticsUnderlineWarning gui=undercurl guisp=" .. lsp.warning },
+		{ "ColorScheme", "*", "highlight LspDiagnosticsUnderlineInformation gui=undercurl guisp=" .. lsp.information },
+		{ "ColorScheme", "*", "highlight LspDiagnosticsUnderlineHint gui=undercurl guisp=" .. lsp.hint },
 	},
 }
 
---
+++ lua/nebula/colors.lua
@@ -0,0 +1,16 @@
+local M = {}
+
+M.git = {
+	added = "#9ece6a",
+	modified = "#2ac3de",
+	removed = "#f7768e",
+}
+
+M.lsp = {
+	error = "#db4b4b",
+	warning = "#e0af68",
+	information = "#0db9d7",
+	hint = "#10b981",
+}
+
+return M
