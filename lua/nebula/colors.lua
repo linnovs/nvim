f3ac670174669914a2449709f9a718482f5af141
Linnovs Liu <7882261+linnovs@users.noreply.github.com>
2021-09-26 01:42:05 +0800

    feat(color): add config for colors

--- lua/nebula/colors.lua
+++ lua/nebula/colors.lua
@@ -1,9 +1,31 @@
 local M = {}
 
+M.basic = {
+	red = "#f7768e", -- 	This keyword, HTML elements, Regex group symbol, CSS units, Terminal Red
+	orange = "#ff9e64", -- 	Number and Boolean constants, Language support constants
+	yellow = "#e0af68", -- 	Function parameters, Regex character sets, Terminal Yellow
+	dark_green = "#9ece6a", -- 	Strings, CSS class names
+	green = "#73daca", -- 	Object literal keys, Markdown links, Terminal Green
+	light_green = "#b4f9f8", -- 	Regex literal strings
+	dark_cyan = "#2ac3de", -- 	Language support functions, CSS HTML elements
+	cyan = "#7dcfff", -- 	Object properties, Regex quantifiers and flags, Markdown headings, Terminal Cyan, Markdown code, Import/export keywords
+	blue = "#7aa2f7", -- 	Function names, CSS property names, Terminal Blue
+	magenta = "#bb9af7", -- 	Control Keywords, Storage Types, Regex symbols and operators, HTML Attributes, Terminal Magenta
+	white = "#c0caf5", -- 	Variables, Class names, Terminal White
+	ligth_gray = "#9aa5ce", -- 	Markdown Text, HTML Text
+	light_yellow = "#cfc9c2", -- 	Parameters inside functions (semantic highlighting only)
+	gray = "#565f89", -- 	Comments
+	black = "#414868", -- 	Terminal Black
+	fg = "#a9b1d6", -- 	Editor Foreground
+	bg = "#24283b", -- 	Editor Background (Storm)
+	alt_bg = "#1f2335",
+	bg_highlight = "#292e42",
+}
+
 M.git = {
-	added = "#9ece6a",
-	modified = "#2ac3de",
-	removed = "#f7768e",
+	added = "#449dab",
+	modified = "#6183bb",
+	removed = "#914c54",
 }
 
 M.lsp = {
