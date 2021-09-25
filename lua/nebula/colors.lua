8b9283bc024cfc4bda998204ed1b014d05bc1baa
Linnovs Liu <7882261+linnovs@users.noreply.github.com>
2021-09-26 02:17:02 +0800

    feat(color): add dark_orange & ligth_blue

--- lua/nebula/colors.lua
+++ lua/nebula/colors.lua
@@ -1,23 +1,26 @@
 local M = {}
 
+-- color mostly from tokyo-night storm
 M.basic = {
-	red = "#f7768e", -- 	This keyword, HTML elements, Regex group symbol, CSS units, Terminal Red
-	orange = "#ff9e64", -- 	Number and Boolean constants, Language support constants
-	yellow = "#e0af68", -- 	Function parameters, Regex character sets, Terminal Yellow
-	dark_green = "#9ece6a", -- 	Strings, CSS class names
-	green = "#73daca", -- 	Object literal keys, Markdown links, Terminal Green
-	light_green = "#b4f9f8", -- 	Regex literal strings
-	dark_cyan = "#2ac3de", -- 	Language support functions, CSS HTML elements
-	cyan = "#7dcfff", -- 	Object properties, Regex quantifiers and flags, Markdown headings, Terminal Cyan, Markdown code, Import/export keywords
-	blue = "#7aa2f7", -- 	Function names, CSS property names, Terminal Blue
-	magenta = "#bb9af7", -- 	Control Keywords, Storage Types, Regex symbols and operators, HTML Attributes, Terminal Magenta
-	white = "#c0caf5", -- 	Variables, Class names, Terminal White
-	ligth_gray = "#9aa5ce", -- 	Markdown Text, HTML Text
-	light_yellow = "#cfc9c2", -- 	Parameters inside functions (semantic highlighting only)
-	gray = "#565f89", -- 	Comments
-	black = "#414868", -- 	Terminal Black
-	fg = "#a9b1d6", -- 	Editor Foreground
-	bg = "#24283b", -- 	Editor Background (Storm)
+	red = "#f7768e",
+	orange = "#ff9e64",
+	dark_orange = "#cc7e50",
+	yellow = "#e0af68",
+	dark_green = "#9ece6a",
+	green = "#73daca",
+	light_green = "#b4f9f8",
+	dark_cyan = "#2ac3de",
+	cyan = "#7dcfff",
+	blue = "#7aa2f7",
+	ligth_blue = "#64c5ff",
+	magenta = "#bb9af7",
+	white = "#c0caf5",
+	ligth_gray = "#9aa5ce",
+	light_yellow = "#cfc9c2",
+	gray = "#565f89",
+	black = "#414868",
+	fg = "#a9b1d6",
+	bg = "#24283b",
 	alt_bg = "#1f2335",
 	bg_highlight = "#292e42",
 }
