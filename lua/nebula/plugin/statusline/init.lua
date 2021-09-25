73721b839d851ea24620d33cff1b07b9e6f7e7cd
Linnovs Liu <7882261+linnovs@users.noreply.github.com>
2021-09-26 01:45:18 +0800

    feat(statusline): add vi_mode component

--- lua/nebula/plugin/statusline/init.lua
+++ lua/nebula/plugin/statusline/init.lua
@@ -1,5 +1,14 @@
 local components = { active = { {}, {}, {} }, inactive = { {}, {}, {} } }
 
+local function insert(position, component, inactive)
+	local pos = { left = 1, mid = 2, right = 3 }
+	local state = inactive and "inactive" or "active"
+	table.insert(components[state][pos[position]], component)
+end
+
+local vi_mode = require("nebula.plugin.statusline.vi_mode")
+insert("left", vi_mode)
+
 local colors = require("nebula.plugin.statusline.colors")
 
 require("feline").setup({
