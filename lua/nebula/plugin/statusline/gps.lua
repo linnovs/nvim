bd07290d18ff69cc15c3a2883d11f4aec6e95046
Linnovs Liu <7882261+linnovs@users.noreply.github.com>
2021-10-09 07:53:34 +0800

    fix(statusline): enabled instead enable

--- lua/nebula/plugin/statusline/gps.lua
+++ lua/nebula/plugin/statusline/gps.lua
@@ -1,10 +1,8 @@
 local gps = require("nvim-gps")
 
 return {
-	provider = function()
-		return gps.get_location()
-	end,
-	enable = gps.is_available,
+	provider = gps.get_location,
+	enabled = gps.is_available,
 	hl = {
 		fg = "gps_fg",
 		bg = "gps_bg",
