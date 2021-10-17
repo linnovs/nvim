37daec3157400d3da412404a7a062885a4f207e2
Linnovs Liu <7882261+linnovs@users.noreply.github.com>
2021-10-17 22:20:09 +0800

    feat(format): add rustfmt

--- lua/nebula/plugin/format.lua
+++ lua/nebula/plugin/format.lua
@@ -1,6 +1,7 @@
 require("format").setup({
 	["*"] = { { cmd = { [[sed -i 's/[ \t]*$//']] } } },
 	lua = { { cmd = { [[stylua]] } } },
+	rust = { { cmd = { [[rustfmt]] } } },
 })
 
 require("nebula.nvim_augroup").creates({
