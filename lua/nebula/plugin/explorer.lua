acfc70bd6bf336dae66658008d6c8af124d67108
Linnovs Liu <7882261+linnovs@users.noreply.github.com>
2021-10-18 22:33:45 +0800

    fix(explorer): change config after update

--- lua/nebula/plugin/explorer.lua
+++ lua/nebula/plugin/explorer.lua
@@ -93,7 +93,15 @@ require("nvim-tree").setup({
 	-- updates the root directory of the tree on `DirChanged` (when your run `:cd` usually)
 	update_cwd = true,
 	-- show lsp diagnostics in the signcolumn
-	lsp_diagnostics = true,
+	diagnostic = {
+		enable = true,
+		icons = {
+			error = "",
+			warning = "",
+			hint = "",
+			info = "",
+		},
+	},
 	-- update the focused file on `BufEnter`, un-collapses the folders recursively until it finds the file
 	update_focused_file = {
 		-- enables the feature
