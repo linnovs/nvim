85e69ca2255e5439bcf55acdfa42be07704c75ed
Linnovs Liu <7882261+linnovs@users.noreply.github.com>
2021-10-22 14:21:10 +0800

    feat(format): add toml format with prettier

--- lua/nebula/plugin/format.lua
+++ lua/nebula/plugin/format.lua
@@ -2,6 +2,7 @@ require("format").setup({
 	["*"] = { { cmd = { [[sed -i 's/[ \t]*$//']] } } },
 	lua = { { cmd = { [[stylua]] } } },
 	rust = { { cmd = { [[rustfmt]] } } },
+	toml = { { cmd = { [[prettier -w]] } } },
 })
 
 require("nebula.nvim_augroup").creates({
