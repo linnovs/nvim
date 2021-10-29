2687152a1c27b1bac5d58575f501e2292b8c8fd7
Linnovs Liu <7882261+linnovs@users.noreply.github.com>
2021-10-29 20:00:25 +0800

    fix(nvim-cmp): add back luasnip super-tab like mapping

--- lua/nebula/plugin/completion.lua
+++ lua/nebula/plugin/completion.lua
@@ -23,6 +23,32 @@ cmp.setup({
 		["<C-Space>"] = cmp.mapping(cmp.mapping.complete(), { "i", "c" }),
 		["<C-y>"] = cmp.config.disable,
 		["<CR>"] = cmp.mapping.confirm({ select = true }),
+
+		["<Tab>"] = cmp.mapping(function(fallback)
+			if cmp.visible() then
+				cmp.select_next_item()
+			elseif luasnip.expand_or_jumpable() then
+				luasnip.expand_or_jump()
+			else
+				fallback()
+			end
+		end, {
+			"i",
+			"s",
+		}),
+
+		["<S-Tab>"] = cmp.mapping(function(fallback)
+			if cmp.visible() then
+				cmp.select_prev_item()
+			elseif luasnip.jumpable(-1) then
+				luasnip.jump(-1)
+			else
+				fallback()
+			end
+		end, {
+			"i",
+			"s",
+		}),
 	},
 	formatting = {
 		format = function(entry, vim_item)
