5d24d3c6b89cf8067b93d5c7bbe30b9bcd6510fa
Linnovs Liu <7882261+linnovs@users.noreply.github.com>
2021-11-16 11:48:34 +0800

    chore(telescope): clean-up config

--- lua/nebula/plugin/telescope.lua
+++ lua/nebula/plugin/telescope.lua
@@ -5,23 +5,6 @@ local vimp = require("vimp")
 
 telescope.setup({
 	defaults = {
-		vimgrep_arguments = {
-			"rg",
-			"--color=never",
-			"--no-heading",
-			"--with-filename",
-			"--line-number",
-			"--column",
-			"--smart-case",
-			"--hidden",
-		},
-		prompt_prefix = "> ",
-		selection_caret = "> ",
-		entry_prefix = "  ",
-		initial_mode = "insert",
-		selection_strategy = "reset",
-		sorting_strategy = "descending",
-		layout_strategy = "horizontal",
 		layout_config = {
 			horizontal = {
 				mirror = false,
@@ -30,38 +13,18 @@ telescope.setup({
 				mirror = false,
 			},
 		},
-		file_sorter = require("telescope.sorters").get_fuzzy_file,
-		file_ignore_patterns = {},
-		generic_sorter = require("telescope.sorters").get_generic_fuzzy_sorter,
-		winblend = 0,
-		border = {},
-		borderchars = { "─", "│", "─", "│", "╭", "╮", "╯", "╰" },
-		color_devicons = true,
-		use_less = true,
-		path_display = {},
-		set_env = { ["COLORTERM"] = "truecolor" }, -- default = nil,
-		file_previewer = require("telescope.previewers").vim_buffer_cat.new,
-		grep_previewer = require("telescope.previewers").vim_buffer_vimgrep.new,
-		qflist_previewer = require("telescope.previewers").vim_buffer_qflist.new,
+		set_env = { ["COLORTERM"] = "truecolor" },
 		mappings = {
 			i = {
 				["<ESC>"] = actions.close,
 			},
 		},
-
-		-- Developer configurations: Not meant for general override
-		buffer_previewer_maker = require("telescope.previewers").buffer_previewer_maker,
 	},
 	pickers = {
 		find_files = {
 			theme = "dropdown",
-			find_command = {
-				"rg",
-				"--ignore",
-				"--hidden",
-				"--files",
-				"--follow",
-			},
+			hidden = true,
+			follow = true,
 		},
 		live_grep = {
 			theme = "dropdown",
