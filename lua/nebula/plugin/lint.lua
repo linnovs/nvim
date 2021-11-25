822dc401f2b7254577e4eaa25d474a7fec741a31
Linnovs Liu <7882261+linnovs@users.noreply.github.com>
2021-11-25 10:10:41 +0800

    fix(lint): golangci-lint out-format instead output-format

--- lua/nebula/plugin/lint.lua
+++ lua/nebula/plugin/lint.lua
@@ -3,10 +3,11 @@ local null_ls = require("null-ls")
 local sources = {
 	null_ls.builtins.code_actions.gitsigns,
 	null_ls.builtins.diagnostics.golangci_lint.with({
-		args = { "run", "--fix=false", "--output-format=json", "$DIRNAME", "--path-prefix", "$ROOT" },
+		args = { "run", "--fix=false", "--out-format=json", "$DIRNAME", "--path-prefix", "$ROOT" },
 	}),
 }
 
 null_ls.config({
+	diagnostic_format = "[#{c}] #{m}",
 	sources = sources,
 })
