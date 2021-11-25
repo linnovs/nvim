4a31f4121fd6f78d20517cf06aadd7b900e6274d
Linnovs Liu <7882261+linnovs@users.noreply.github.com>
2021-11-25 11:27:52 +0800

    chore(completion): remove unused function

--- lua/nebula/plugin/completion.lua
+++ lua/nebula/plugin/completion.lua
@@ -1,15 +1,3 @@
-local has_words_before = function()
-	if vim.api.nvim_buf_get_option(0, "buftype") == "prompt" then
-		return false
-	end
-	local line, col = unpack(vim.api.nvim_win_get_cursor(0))
-	return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
-end
-
-local feedkey = function(key)
-	vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes(key, true, true, true), "n", true)
-end
-
 local luasnip = require("luasnip")
 local cmp = require("cmp")
 
