7a961b1333dc4c08e008274dc81c44dc68d18107
Linnovs Liu <7882261+linnovs@users.noreply.github.com>
2021-12-13 09:51:39 +0800

    fix(trouble): use diagnostic_sign instead of lsp_diagnostic_sign

--- lua/nebula/plugin/trouble.lua
+++ lua/nebula/plugin/trouble.lua
@@ -5,7 +5,7 @@ require("trouble").setup({
 	height = 10, -- height of the trouble list when position is top or bottom
 	width = 50, -- width of the list when position is left or right
 	icons = true, -- use devicons for filenames
-	mode = "lsp_workspace_diagnostics", -- "lsp_workspace_diagnostics", "lsp_document_diagnostics", "quickfix", "lsp_references", "loclist"
+	mode = "workspace_diagnostics", -- "lsp_workspace_diagnostics", "lsp_document_diagnostics", "quickfix", "lsp_references", "loclist"
 	fold_open = "", -- icon used for open folds
 	fold_closed = "", -- icon used for closed folds
 	action_keys = { -- key mappings for actions in the trouble list
@@ -42,7 +42,7 @@ require("trouble").setup({
 		information = "",
 		other = "﫠",
 	},
-	use_lsp_diagnostic_signs = true,
+	use_diagnostic_signs = true,
 })
 
 vimp.nnoremap("<Leader>xx", "<CMD>TroubleToggle<CR>")
