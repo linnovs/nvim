b9b5011f88b7c8d4010ff450e13c98a0d9c014c1
Linnovs Liu <7882261+linnovs@users.noreply.github.com>
2021-12-26 00:40:24 +0800

    feat(indentGuide): add ft packer to exclude

--- lua/nebula/plugin/indent-guideline.lua
+++ lua/nebula/plugin/indent-guideline.lua
@@ -1,7 +1,7 @@
 require("indent_blankline").setup({
 	char = "",
 	buftype_exclude = { "terminal", "help" },
-	filetype_exclude = { "lsp-installer", "alpha" },
+	filetype_exclude = { "lsp-installer", "alpha", "packer" },
 	space_char_blankline = " ",
 	show_current_context = true,
 	use_treesitter = true,
