d62ce4660d0e1236e3ecfaf592272837c38be61a
Linnovs Liu <7882261+linnovs@users.noreply.github.com>
2021-12-31 11:06:55 +0800

    feat(packer): add auto update (every 7 days)

---
+++ lua/nebula/autoupdate.lua
@@ -0,0 +1,42 @@
+local cache = vim.fn.stdpath("cache")
+local flock_path = cache .. "/packer.autoupdate.lock"
+
+local function should_update()
+	local file = io.open(cache .. "/packer.autoupdate", "r+")
+
+	if not file then
+		file = io.open(cache .. "/packer.autoupdate", "w+")
+	end
+
+	local now = os.time()
+	local last_update
+
+	local content = file:read("*l")
+	if not content then
+		last_update = now
+	else
+		last_update = now - os.time(os.date("*t", tonumber(content)))
+	end
+
+	if last_update > 7 * 86400 then
+		file:seek("set")
+		file:write(now)
+		file:flush()
+		file:close()
+
+		return true
+	end
+
+	return false
+end
+
+-- don't run if the file lock exist
+if 0 < os.execute("mkdir " .. flock_path .. " >/dev/null 2>&1") then
+	return
+end
+
+if should_update() then
+	vim.cmd("PackerSync")
+end
+
+os.execute("rmdir " .. flock_path)
