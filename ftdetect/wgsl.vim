43414dce84c0e2971e8dfd67fef922420e2b71ae
Linnovs Liu <7882261+linnovs@users.noreply.github.com>
2022-01-15 23:15:15 +0800

    feat(treesitter): add wgsl

---
+++ ftdetect/wgsl.vim
@@ -0,0 +1,1 @@
+autocmd BufRead,BufNewFile *.wgsl set filetype=wgsl
--- lua/nebula/plugin/treesitter.lua
+++ lua/nebula/plugin/treesitter.lua
@@ -1,3 +1,13 @@
+local parser_config = require("nvim-treesitter.parsers").get_parser_configs()
+
+parser_config.wgsl = {
+	install_info = {
+		url = "https://github.com/szebniok/tree-sitter-wgsl",
+		files = { "src/parser.c" },
+	},
+	filetype = "wgsl",
+}
+
 require("nvim-treesitter.configs").setup({
 	ensure_installed = "maintained",
 	ignore_install = {},
