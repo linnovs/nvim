1016dae84b1d141ad3cadf3d64ffb326ce99663b
Linnovs Liu <7882261+linnovs@users.noreply.github.com>
2022-04-28 10:38:20 +0800

    chore(tressiter): remove custom wgsl config

--- lua/nebula/plugin/treesitter.lua
+++ lua/nebula/plugin/treesitter.lua
@@ -1,13 +1,3 @@
-local parser_config = require("nvim-treesitter.parsers").get_parser_configs()
-
-parser_config.wgsl = {
-	install_info = {
-		url = "https://github.com/szebniok/tree-sitter-wgsl",
-		files = { "src/parser.c" },
-	},
-	filetype = "wgsl",
-}
-
 require("nvim-treesitter.configs").setup({
 	ensure_installed = "all",
 	ignore_install = {},
