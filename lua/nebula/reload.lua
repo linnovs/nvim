55225d98bb6c1aa4e0399ca0c2330a09a7734f66
Linnovs Liu <7882261+linnovs@users.noreply.github.com>
2022-04-30 20:04:48 +0800

    refactor: change reload config

--- lua/nebula/reload.lua
+++ lua/nebula/reload.lua
@@ -4,15 +4,13 @@ vimp.map_command("Reload", function()
 	vimp.unmap_all()
 
 	for key, _ in pairs(package.loaded) do
-		if key == "nebula" or key:sub(1, #"nebula.") == "nebula." or key == "plugins" then
+		if key:match("^nebula") then
 			package.loaded[key] = nil
 		end
 	end
 
 	vim.cmd("silent wa")
-
-	dofile(vim.fn.stdpath("config") .. "/init.lua")
-	vim.cmd("PackerSync")
-
-	print("Reloaded config")
+	dofile(vim.env.MYVIMRC)
+	vim.cmd("PackerCompile")
+	vim.notify("Reloaded config")
 end)
