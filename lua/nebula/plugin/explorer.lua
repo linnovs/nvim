7dd79de8fff7f5859e59abbb841061a7406f220b
Linnovs Liu <7882261+linnovs@users.noreply.github.com>
2022-05-03 01:36:27 +0800

    fix(explorer): add '$' to the end of pattern

--- lua/nebula/plugin/explorer.lua
+++ lua/nebula/plugin/explorer.lua
@@ -115,7 +115,7 @@ require("nvim-tree").setup({
 	},
 	filters = {
 		dotfiles = false,
-		custom = { "^.git", "^node_modules", "^.cache" },
+		custom = { "^.git$", "^node_modules$", "^.cache$" },
 	},
 	actions = {
 		open_file = {
