8b5a88e5ff8dfa433b7aecd00da59ebc2d7228bf
Linnovs Liu <7882261+linnovs@users.noreply.github.com>
2022-06-10 03:43:56 +0800

    chore(cmp): reconfigurate

--- lua/nebula/plugin/completion.lua
+++ lua/nebula/plugin/completion.lua
@@ -5,71 +5,71 @@ local cmp_autopairs = require("nvim-autopairs.completion.cmp")
 cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done({ map_char = { tex = "" } }))
 
 luasnip.config.setup({
-	delete_check_events = "TextChanged,InsertLeave",
-	region_check_events = "CursorMoved",
+    delete_check_events = "TextChanged,InsertLeave",
+    region_check_events = "CursorMoved",
 })
 
 cmp.setup({
-	snippet = {
-		expand = function(args)
-			luasnip.lsp_expand(args.body)
-		end,
-	},
-	mapping = cmp.mapping.preset.insert({
-		["<C-Space>"] = cmp.mapping(cmp.mapping.complete(), { "i", "c" }),
-		["<C-y>"] = cmp.config.disable,
-		["<CR>"] = cmp.mapping.confirm({ select = true }),
+    snippet = {
+        expand = function(args)
+            luasnip.lsp_expand(args.body)
+        end,
+    },
+    mapping = cmp.mapping.preset.insert({
+        ["<C-Space>"] = cmp.mapping(cmp.mapping.complete(), { "i", "c" }),
+        ["<C-y>"] = cmp.config.disable,
+        ["<CR>"] = cmp.mapping.confirm({ select = true }),
 
-		["<Tab>"] = cmp.mapping(function(fallback)
-			if luasnip.expand_or_locally_jumpable() then
-				luasnip.expand_or_jump()
-			else
-				fallback()
-			end
-		end, {
-			"i",
-			"s",
-		}),
+        ["<Tab>"] = cmp.mapping(function(fallback)
+            if luasnip.expand_or_locally_jumpable() then
+                luasnip.expand_or_jump()
+            else
+                fallback()
+            end
+        end, {
+            "i",
+            "s",
+        }),
 
-		["<S-Tab>"] = cmp.mapping(function(fallback)
-			if luasnip.jumpable(-1) then
-				luasnip.jump(-1)
-			else
-				fallback()
-			end
-		end, {
-			"i",
-			"s",
-		}),
-	}),
-	formatting = {
-		format = function(entry, vim_item)
-			-- fancy icons and a name of kind
-			vim_item.kind = require("lspkind").presets.default[vim_item.kind] .. " " .. vim_item.kind
+        ["<S-Tab>"] = cmp.mapping(function(fallback)
+            if luasnip.jumpable(-1) then
+                luasnip.jump(-1)
+            else
+                fallback()
+            end
+        end, {
+            "i",
+            "s",
+        }),
+    }),
+    formatting = {
+        format = function(entry, vim_item)
+            -- fancy icons and a name of kind
+            vim_item.kind = require("lspkind").presets.default[vim_item.kind] .. " " .. vim_item.kind
 
-			-- set a name for each source
-			vim_item.menu = ({
-				nvim_lsp = "[LSP]",
-				luasnip = "[LuaSnip]",
-				buffer = "[Buffer]",
-				path = "[Path]",
-				spell = "[Spell]",
-				nvim_lua = "[Lua]",
-			})[entry.source.name]
-			return vim_item
-		end,
-	},
-	sources = cmp.config.sources({
-		{ name = "nvim_lsp" },
-		{ name = "luasnip" },
-	}, {
-		{ name = "buffer" },
-		{ name = "path" },
-		{ name = "spell" },
-		{ name = "nvim_lua" },
-	}, {
-		{ name = "git" },
-	}),
+            -- set a name for each source
+            vim_item.menu = ({
+                nvim_lsp = "[LSP]",
+                luasnip = "[LuaSnip]",
+                buffer = "[Buffer]",
+                path = "[Path]",
+                spell = "[Spell]",
+                nvim_lua = "[Lua]",
+            })[entry.source.name]
+            return vim_item
+        end,
+    },
+    sources = cmp.config.sources({
+        { name = "nvim_lsp" },
+        { name = "luasnip" },
+    }, {
+        { name = "buffer" },
+        { name = "path" },
+        { name = "spell" },
+        { name = "nvim_lua" },
+    }, {
+        { name = "git" },
+    }),
 })
 
 require("cmp_git").setup()
