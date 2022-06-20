b2f0166ad54933a6b848341a1608cf076c9b0dcb
Linnovs Liu <7882261+linnovs@users.noreply.github.com>
2022-06-20 14:44:51 +0800

    style(telescope): change tab to space

--- lua/nebula/plugin/telescope.lua
+++ lua/nebula/plugin/telescope.lua
@@ -5,68 +5,60 @@ local z_config = require("telescope._extensions.zoxide.config")
 local vimp = require("vimp")
 
 telescope.setup({
-	defaults = {
-		layout_config = {
-			horizontal = {
-				mirror = false,
-			},
-			vertical = {
-				mirror = false,
-			},
-		},
-		set_env = { ["COLORTERM"] = "truecolor" },
-		mappings = {
-			i = {
-				["<ESC>"] = actions.close,
-				["<C-h>"] = "which_key",
-			},
-		},
-	},
-	pickers = {
-		find_files = {
-			theme = "dropdown",
-			hidden = true,
-			follow = true,
-		},
-		live_grep = {
-			theme = "dropdown",
-		},
-		buffers = {
-			theme = "dropdown",
-		},
-		help_tags = {
-			theme = "dropdown",
-		},
-		zoxide = {
-			theme = "dropdown",
-		},
-	},
+    defaults = {
+        layout_config = {
+            horizontal = {
+                mirror = false,
+            },
+            vertical = {
+                mirror = false,
+            },
+        },
+        set_env = { ["COLORTERM"] = "truecolor" },
+        mappings = {
+            i = {
+                ["<ESC>"] = actions.close,
+                ["<C-h>"] = "which_key",
+            },
+        },
+    },
+    pickers = {
+        find_files = {
+            theme = "dropdown",
+            hidden = true,
+            follow = true,
+        },
+        live_grep = {
+            theme = "dropdown",
+        },
+        buffers = {
+            theme = "dropdown",
+        },
+        help_tags = {
+            theme = "dropdown",
+        },
+        zoxide = {
+            theme = "dropdown",
+        },
+    },
 })
 
 telescope.load_extension("zoxide")
 
 z_config.setup({
-	mappings = {
-		default = {
-			after_action = function(selection)
-				print("Directory changed to " .. selection.path)
-				vim.cmd("NvimTreeToggle")
-			end,
-		},
-	},
 })
 
 vimp.nnoremap("<Leader><Leader>", function()
-	builtin.find_files()
+    builtin.find_files()
 end)
 vimp.nnoremap("<Leader>fg", function()
-	builtin.live_grep()
+    builtin.live_grep()
 end)
 vimp.nnoremap("<Leader>fb", function()
-	builtin.buffers()
+    builtin.buffers()
 end)
 vimp.nnoremap("<Leader>fh", function()
-	builtin.help_tags()
+    builtin.help_tags()
 end)
 vimp.nnoremap("<Leader>todo", "<CMD>TodoTelescope<CR>")
 vimp.nnoremap("<Leader>z", "<CMD>Telescope zoxide list<CR>")
