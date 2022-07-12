8ef45e259be3aa2881d8713cd44ee9ca3fdbaca1
Linnovs Liu <7882261+linnovs@users.noreply.github.com>
2022-07-12 08:03:04 +0800

    style(lsp/on_attach): use space instead of tab

--- lua/nebula/lsp/on_attach.lua
+++ lua/nebula/lsp/on_attach.lua
@@ -2,42 +2,42 @@ local vimp = require("vimp")
 local builtin = require("telescope.builtin")
 
 return function(client, bufnr)
-	-- enable completion
-	vim.api.nvim_buf_set_option(bufnr, "omnifunc", "v:lua.vim.lsp.omnifunc")
+    -- enable completion
+    vim.api.nvim_buf_set_option(bufnr, "omnifunc", "v:lua.vim.lsp.omnifunc")
 
-	vimp.add_buffer_maps(bufnr, function()
-		vimp.nnoremap({ "override" }, "gD", vim.lsp.buf.declaration)
-		vimp.nnoremap({ "override" }, "gd", builtin.lsp_definitions)
-		vimp.nnoremap({ "override" }, "gi", builtin.lsp_implementations)
-		vimp.nnoremap({ "override" }, "K", vim.lsp.buf.hover)
-		vimp.nnoremap({ "override" }, "<C-k>", vim.lsp.buf.signature_help)
-		vimp.nnoremap({ "override" }, "<Leader>rn", vim.lsp.buf.rename)
-		vimp.nnoremap({ "override" }, "<Leader>cam", ":CodeActionMenu<CR>")
-		vimp.nnoremap({ "override" }, "<Leader>D", builtin.lsp_type_definitions)
-		vimp.nnoremap({ "override" }, "gr", builtin.lsp_references)
-		vimp.nnoremap({ "override" }, "<Leader>ds", builtin.lsp_document_symbols)
-	end)
+    vimp.add_buffer_maps(bufnr, function()
+        vimp.nnoremap({ "override" }, "gD", vim.lsp.buf.declaration)
+        vimp.nnoremap({ "override" }, "gd", builtin.lsp_definitions)
+        vimp.nnoremap({ "override" }, "gi", builtin.lsp_implementations)
+        vimp.nnoremap({ "override" }, "K", vim.lsp.buf.hover)
+        vimp.nnoremap({ "override" }, "<C-k>", vim.lsp.buf.signature_help)
+        vimp.nnoremap({ "override" }, "<Leader>rn", vim.lsp.buf.rename)
+        vimp.nnoremap({ "override" }, "<Leader>cam", ":CodeActionMenu<CR>")
+        vimp.nnoremap({ "override" }, "<Leader>D", builtin.lsp_type_definitions)
+        vimp.nnoremap({ "override" }, "gr", builtin.lsp_references)
+        vimp.nnoremap({ "override" }, "<Leader>ds", builtin.lsp_document_symbols)
+    end)
 
-	if client.resolved_capabilities.document_formatting then
-		vim.cmd([[
+    if client.resolved_capabilities.document_formatting then
+        vim.cmd([[
             augroup LspFormatting
                 autocmd! * <buffer>
                 autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting_seq_sync()
             augroup END
         ]])
-	end
+    end
 
-	if client.resolved_capabilities.code_lens then
-		vim.cmd([[
+    if client.resolved_capabilities.code_lens then
+        vim.cmd([[
             augroup LspCodeLens
                 autocmd! * <buffer>
                 autocmd CursorHold,CursorHoldI,InsertLeave <buffer> lua vim.lsp.codelens.refresh()
             augroup END
         ]])
-		vimp.add_buffer_maps(bufnr, function()
-			vimp.nnoremap({ "override" }, "<Leader>cl", vim.lsp.codelens.run)
-		end)
-	end
+        vimp.add_buffer_maps(bufnr, function()
+            vimp.nnoremap({ "override" }, "<Leader>cl", vim.lsp.codelens.run)
+        end)
+    end
 
     if client.resolved_capabilities.document_symbol then
         require("nvim-navic").attach(client, bufnr)
