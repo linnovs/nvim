f3c147122fbdc6971be416d67cd9bbfd3eb42f1a
Linnovs Liu <7882261+linnovs@users.noreply.github.com>
2023-01-17 21:43:01 +0800

    feat(neo-tree): auto close after open file

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -175,6 +175,15 @@ return {
 					folder_empty = " ",
 				},
 			},
+			event_handlers = {
+				{
+					event = "file_opened",
+					handler = function()
+						-- auto close
+						require("neo-tree").close_all()
+					end,
+				},
+			},
 		},
 	},
 
