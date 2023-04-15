0780410548096eac14c0005e2dd73329a70309ae
Linnovs Liu <fforwm_cynnwys@slmail.me>
2023-04-15 21:45:45 +0800

    feat(null_ls): add languagetool for code_actions & diagnostics

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -157,6 +157,8 @@ return {
 				diagnostic_format = "[#{c}] #{m}",
 				sources = {
 					null_ls.builtins.code_actions.gitsigns,
+					null_ls.builtins.code_actions.ltrs,
+					null_ls.builtins.diagnostics.ltrs,
 					null_ls.builtins.diagnostics.golangci_lint.with({
 						args = { "run", "--fix=false", "--out-format=json", "$DIRNAME", "--path-prefix", "$ROOT" },
 					}),
