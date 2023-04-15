cb49f5399550ab0840b9f060d9832cdb6182f9e4
Linnovs Liu <fforwm_cynnwys@slmail.me>
2023-04-15 21:04:01 +0800

    feat(lsp): convert to use LspAttach autocmd

--- lua/kuuga/config/lsp.lua
+++ lua/kuuga/config/lsp.lua
@@ -34,61 +34,70 @@ function M.disable_diagnostic(name, fts)
 	M.diagnostic_filter[name] = fts
 end
 
-M.on_attach = function(client, bufnr)
-	-- enable completion
-	vim.api.nvim_buf_set_option(bufnr, "omnifunc", "v:lua.vim.lsp.omnifunc")
+vim.api.nvim_create_autocmd("LspAttach", {
+	group = vim.api.nvim_create_augroup("KuugaLspConfig", {}),
+	callback = function(ev)
+		local bufnr = ev.buf
+		local client = vim.lsp.get_client_by_id(ev.data.client_id)
 
-	local function map(mode, lhs, rhs, desc)
-		vim.keymap.set(mode, lhs, rhs, { buffer = bufnr, desc = desc })
-	end
+		local function map(mode, lhs, rhs, desc)
+			vim.keymap.set(mode, lhs, rhs, { buffer = bufnr, desc = desc })
+		end
 
-	map("n", "gD", vim.lsp.buf.declaration, "Go to declaration")
-	map("n", "gd", builtin.lsp_definitions, "Go to definitions")
-	map("n", "gi", builtin.lsp_implementations, "Go to implementations")
-	map("n", "K", vim.lsp.buf.hover, "Hover")
-	map("n", "<Leader>rn", vim.lsp.buf.rename, "Rename")
-	map("n", "<Leader>D", builtin.lsp_type_definitions, "Type definitions")
-	map("n", "gr", builtin.lsp_references, "References")
+		map("n", "gD", vim.lsp.buf.declaration, "Go to declaration")
+		map("n", "gd", builtin.lsp_definitions, "Go to definitions")
+		map("n", "gi", builtin.lsp_implementations, "Go to implementations")
+		map("n", "K", vim.lsp.buf.hover, "Hover")
+		map("n", "<Leader>rn", vim.lsp.buf.rename, "Rename")
+		map("n", "<Leader>D", builtin.lsp_type_definitions, "Type definitions")
+		map("n", "gr", builtin.lsp_references, "References")
 
-	if client.server_capabilities.codeActionProvider then
-		map("n", "<Leader>cam", ":CodeActionMenu<CR>", "Code action")
-	end
+		if client.server_capabilities.codeActionProvider then
+			map("n", "<Leader>cam", ":CodeActionMenu<CR>", "Code action")
+		end
 
-	if client.supports_method("textDocument/formatting") then
-		vim.api.nvim_create_autocmd("BufWritePre", {
-			group = vim.api.nvim_create_augroup("LspFormat." .. bufnr, { clear = false }),
-			buffer = bufnr,
-			callback = function()
-				vim.lsp.buf.format({
-					filter = function(c)
-						return M.format_filter[c.name] == nil
-					end,
-				})
-			end,
-		})
-	end
+		if client.supports_method("textDocument/formatting") then
+			vim.api.nvim_create_autocmd("BufWritePre", {
+				group = vim.api.nvim_create_augroup("LspFormat." .. bufnr .. "." .. client.name, { clear = true }),
+				buffer = bufnr,
+				callback = function()
+					vim.lsp.buf.format({
+						filter = function(c)
+							return M.format_filter[c.name] == nil
+						end,
+					})
+				end,
+			})
+		end
 
-	if has_value(M.diagnostic_filter[client.name], vim.bo[bufnr].filetype) then
-		vim.lsp.handlers["textDocument/publishDiagnostics"] = function() end
-	end
+		if has_value(M.diagnostic_filter[client.name], vim.bo[bufnr].filetype) then
+			vim.lsp.handlers["textDocument/publishDiagnostics"] = function() end
+		end
 
-	if client.server_capabilities.codeLensProvider then
-		vim.api.nvim_create_autocmd({ "CursorHold", "CursorHoldI", "InsertLeave" }, {
-			group = vim.api.nvim_create_augroup("LspCodeLens." .. bufnr, { clear = false }),
-			buffer = bufnr,
-			callback = vim.lsp.codelens.refresh,
-		})
-		map("n", "<Leader>cl", vim.lsp.codelens.run, "Code lens")
-	end
+		if client.server_capabilities.codeLensProvider then
+			vim.api.nvim_create_autocmd({ "CursorHold", "CursorHoldI", "InsertLeave" }, {
+				group = vim.api.nvim_create_augroup("LspCodeLens." .. bufnr .. "." .. client.name, { clear = true }),
+				buffer = bufnr,
+				callback = vim.lsp.codelens.refresh,
+			})
+			map("n", "<Leader>cl", vim.lsp.codelens.run, "Code lens")
+		end
 
-	if client.server_capabilities.documentSymbolProvider then
-		require("nvim-navic").attach(client, bufnr)
-	end
+		if client.server_capabilities.documentSymbolProvider then
+			require("nvim-navic").attach(client, bufnr)
+			require("nvim-navbuddy").attach(client, bufnr)
+			if vim.wo.winbar == "" then
+				vim.wo.winbar = "         %{%v:lua.require'nvim-navic'.get_location()%}"
+			end
+		end
 
-	if client.server_capabilities.signatureHelpProvider then
-		map({ "n", "i" }, "<C-k>", vim.lsp.buf.signature_help, "Signature help")
-		require("lsp_signature").on_attach()
-	end
-end
+		if client.server_capabilities.signatureHelpProvider then
+			map({ "n", "i" }, "<C-k>", vim.lsp.buf.signature_help, "Signature help")
+			-- require("lsp_signature").on_attach()
+		end
+
+		require("lsp-inlayhints").on_attach(client, bufnr)
+	end,
+})
 
 return M
