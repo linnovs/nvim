6acc53e6ca0d3711c1692c820d141ce7a5a20616
Linnovs Liu <fforwm_cynnwys@slmail.me>
2023-04-15 21:55:04 +0800

    chore(null_ls): add plenary as dependencies

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -148,7 +148,7 @@ return {
 	{
 		"jose-elias-alvarez/null-ls.nvim",
 		event = "BufReadPre",
-		dependencies = { "williamboman/mason.nvim" },
+		dependencies = { "williamboman/mason.nvim", "nvim-lua/plenary.nvim" },
 		opts = function()
 			local null_ls = require("null-ls")
 
