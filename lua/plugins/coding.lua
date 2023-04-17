7abb285b4fac5e032d4e3281a3aca7a5e6ec5aa0
Linnovs Liu <fforwm_cynnwys@slmail.me>
2023-04-17 23:35:22 +0800

    feat(cmp): enable cmp on search cmdline mode as well

--- lua/plugins/coding.lua
+++ lua/plugins/coding.lua
@@ -99,7 +99,7 @@ return {
 			local cmp_autopairs = require("nvim-autopairs.completion.cmp")
 
 			cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done({ map_char = { tex = "" } }))
-			cmp.setup.cmdline("/", {
+			cmp.setup.cmdline({ "/", "?" }, {
 				mapping = cmp.mapping.preset.cmdline(),
 				sources = cmp.config.sources({ { name = "buffer" } }),
 			})
