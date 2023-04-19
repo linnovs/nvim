1356b3bc7bc12039feb02029601e202a762c818a
Linnovs Liu <fforwm_cynnwys@slmail.me>
2023-04-19 19:17:31 +0800

    chore(dashboard): replace project icon

--- lua/kuuga/config/dashboard.lua
+++ lua/kuuga/config/dashboard.lua
@@ -35,7 +35,7 @@ dashboard.section.buttons.val = {
 	dashboard.button("n", "  > New file", ":ene <BAR> startinsert <CR>"),
 	dashboard.button("f", "  > Find file", ":Telescope find_files<CR>"),
 	dashboard.button("r", "  > Recent", ":Telescope oldfiles<CR>"),
-	dashboard.button("z", "  > Project", ":Telescope zoxide list<CR>"),
+	dashboard.button("z", "  > Project", ":Telescope zoxide list<CR>"),
 	dashboard.button("s", "  > Settings", ":e $MYVIMRC | :cd %:p:h | lua vim.notify('Changed to nvimrc')<CR>"),
 	dashboard.button("q", "  > Quit NVIM", ":qa<CR>"),
 }
