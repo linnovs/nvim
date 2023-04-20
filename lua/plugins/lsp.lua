0c2c359fff1d255bc2b417071e437a0601e46edf
Linnovs Liu <fforwm_cynnwys@slmail.me>
2023-04-20 12:59:40 +0800

    feat(null-ls): remove trim_newlines,trim_whitespace. add cspell

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -222,9 +222,8 @@ return {
 			local null_ls = require("null-ls")
 
 			return {
-				on_attach = lsp.on_attach,
-				diagnostic_format = "[#{c}] #{m}",
 				sources = {
+					null_ls.builtins.code_actions.cspell,
 					null_ls.builtins.code_actions.gitsigns,
 					null_ls.builtins.code_actions.ltrs,
 					null_ls.builtins.diagnostics.ltrs,
@@ -238,8 +237,6 @@ return {
 					null_ls.builtins.formatting.terraform_fmt,
 					null_ls.builtins.formatting.stylua,
 					null_ls.builtins.formatting.taplo,
-					null_ls.builtins.formatting.trim_whitespace,
-					null_ls.builtins.formatting.trim_newlines,
 				},
 			}
 		end,
