0a5ef713fdeca4a6ea7ec5b19a399b7abe8689c3
Linnovs Liu <fforwm_cynnwys@slmail.me>
2023-04-26 11:57:47 +0800

    fix(Comment): should use config instead of opts

--- lua/plugins/coding.lua
+++ lua/plugins/coding.lua
@@ -1,12 +1,12 @@
 return {
 	{
 		"numToStr/Comment.nvim",
-		opts = {
-			pre_hook = function()
-				return require("ts_context_commentstring.integrations.comment_nvim").create_pre_hook()
-			end,
-		},
 		event = "BufReadPost",
+		config = function()
+			require("Comment").setup({
+				pre_hook = require("ts_context_commentstring.integrations.comment_nvim").create_pre_hook(),
+			})
+		end,
 	},
 
 	{
