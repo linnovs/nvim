06dfa6541f0aa10200e187ff5944d5c3f6186496
Linnovs Liu <fforwm_cynnwys@slmail.me>
2023-04-26 11:57:24 +0800

    feat(null_ls): remove cspell

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -223,7 +223,6 @@ return {
 
 			return {
 				sources = {
-					null_ls.builtins.code_actions.cspell,
 					null_ls.builtins.code_actions.gitsigns,
 					null_ls.builtins.code_actions.ltrs,
 					null_ls.builtins.diagnostics.ltrs,
