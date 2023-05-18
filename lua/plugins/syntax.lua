f00abbce5f68790e67618192b9f27cf2fb54b41f
Linnovs Liu <fforwm_cynnwys@slmail.me>
2023-05-19 00:38:58 +0800

    feat(plugin): add kitty config file syntax hl

--- lua/plugins/syntax.lua
+++ lua/plugins/syntax.lua
@@ -1,4 +1,5 @@
 return {
 	{ "alker0/chezmoi.vim", event = "BufEnter */chezmoi/*" }, -- syntax highlight for chezmoi
 	{ "towolf/vim-helm", ft = "helm" }, -- syntax highlight for helm
+	{ "fladson/vim-kitty", ft = "kitty" }, -- syntax highlight for kitty configure files
 }
