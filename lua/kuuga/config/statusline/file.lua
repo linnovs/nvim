fefb44fcbc6d8994248b602a759f265992e86b5c
Linnovs Liu <fforwm_cynnwys@slmail.me>
2023-05-18 17:41:37 +0800

    chore(statusline): remove file component's separator

--- lua/kuuga/config/statusline/file.lua
+++ lua/kuuga/config/statusline/file.lua
@@ -1,7 +1,7 @@
 local M = {}
 
 M.icon = { "filetype", icon_only = true, padding = { left = 1, right = 0 } }
-M.name = { "filename", symbols = { modified = "  ", readonly = " 🔒" }, separator = " " }
+M.name = { "filename", symbols = { modified = "  ", readonly = " 🔒" } }
 M.location = { "location" }
 M.encoding = { "encoding", separator = "" }
 M.format = { "fileformat", symbols = { unix = " ", dos = " ", mac = " " }, separator = "" }
