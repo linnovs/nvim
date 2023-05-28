cbe0920271d8d4384e28cf9bd86a239cd6d74b4b
Linnovs Liu <fforwm_cynnwys@slmail.me>
2023-05-28 17:31:49 +0800

    feat(options): add background = dark

--- lua/kuuga/options.lua
+++ lua/kuuga/options.lua
@@ -1,3 +1,4 @@
+vim.opt.background = "dark" -- "dark" or "light", used for highlight colors
 vim.opt.clipboard:append("unnamedplus") -- use the clipboard as the unnamed register
 vim.opt.cmdheight = 1 -- number of lines to use for the command-line
 vim.opt.colorcolumn = "+0" -- columns to highlight
