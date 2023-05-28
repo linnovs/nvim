15cca854d91e294d29686322e8656546f44e1e12
Linnovs Liu <fforwm_cynnwys@slmail.me>
2023-05-28 17:39:09 +0800

    chore(cmp): remove duplicated copilot config

--- lua/plugins/coding.lua
+++ lua/plugins/coding.lua
@@ -38,21 +38,6 @@ return {
 		end,
 	},
 
-	{
-		"zbirenbaum/copilot-cmp",
-		dependencies = {
-			"zbirenbaum/copilot.lua",
-		},
-		lazy = true,
-		opts = function()
-			return {
-				formatters = {
-					insert_text = require("copilot_cmp.format").remove_existing,
-				},
-			}
-		end,
-	},
-
 	-- codeium
 	{
 		"jcdickinson/codeium.nvim",
