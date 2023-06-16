8bd5b70cb43576623079e923bb375a2ae0ba4ec6
Linnovs Liu <fforwm_cynnwys@slmail.me>
2023-06-16 14:18:17 +0800

    feat(Octo): change close review tab with mapping <Leader>qq

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -190,12 +190,13 @@ return {
 		"pwntester/octo.nvim",
 		cmd = { "Octo" },
 		opts = {
+			enable_builtin = true,
 			mappings = {
 				submit_win = {
 					approve_review = { lhs = "<leader>ar", desc = "approve review" },
 					comment_review = { lhs = "<leader>cr", desc = "comment review" },
 					request_changes = { lhs = "<leader>rc", desc = "request changes review" },
-					close_review_tab = { lhs = "<leader>x", desc = "close review tab" },
+					close_review_tab = { lhs = "<leader>qq", desc = "close review tab" },
 				},
 			},
 		},
