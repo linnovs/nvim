84e1f45c8aaa309791638afe32082a1b9ffff1fa
Linnovs Liu <fforwm_cynnwys@slmail.me>
2023-07-27 22:50:28 +0800

    feat(lsp): customize how diagnostic are shown

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -22,6 +22,14 @@ return {
 				local hl = "DiagnosticSign" .. type
 				vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
 			end
+
+			vim.diagnostic.config({
+				virtual_text = true,
+				signs = true,
+				underline = true,
+				update_in_insert = true,
+				severity_sort = false,
+			})
 		end,
 		opts = {
 			servers = {
