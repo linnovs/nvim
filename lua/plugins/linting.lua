0812f4ac51b81b05529f81a4747ccc9021cb4e3c
Linnovs Liu <fforwm_cynnwys@slmail.me>
2023-10-14 23:28:55 +0800

    feat(linting): new linting settings

---
+++ lua/plugins/linting.lua
@@ -0,0 +1,40 @@
+return {
+	{
+		"mfussenegger/nvim-lint",
+		event = "BufReadPre",
+		opts = {
+			linters_by_ft = {
+				go = { "golangcilint" },
+			},
+		},
+		config = function(_, opts)
+			local M = {}
+			local lint = require("lint")
+			lint.linters_by_ft = opts.linters_by_ft
+
+			function M.debounce(ms, fn)
+				local timer = vim.loop.new_timer()
+				return function(...)
+					local argv = { ... }
+					timer:start(ms, 0, function()
+						timer:stop()
+						vim.schedule_wrap(fn)(unpack(argv))
+					end)
+				end
+			end
+
+			function M.lint()
+				local names = lint.linters_by_ft[vim.bo.filetype] or {}
+
+				if #names > 0 then
+					lint.try_lint(names)
+				end
+			end
+
+			vim.api.nvim_create_autocmd({ "BufWritePost", "BufReadPost", "InsertLeave" }, {
+				group = vim.api.nvim_create_augroup("nvim-lint", { clear = true }),
+				callback = M.debounce(100, M.lint),
+			})
+		end,
+	},
+}
