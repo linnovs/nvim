6dd550e492100a982216a5c0896e8441bac79b6d
Linnovs Liu <fforwm_cynnwys@slmail.me>
2023-10-14 23:54:31 +0800

    feat(todo-comments): update lazy's cmd & keys

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -30,10 +30,24 @@ return {
 	{
 		"folke/todo-comments.nvim",
 		dependencies = { "nvim-telescope/telescope.nvim" },
-		cmd = "TodoTelescope",
+		cmd = { "TodoTrouble", "TodoTelescope" },
 		event = "BufReadPost",
 		keys = {
 			{ "<leader>todo", "<cmd>TodoTelescope<CR>", desc = "Find TODOs" },
+			{
+				"]t",
+				function()
+					require("todo-comments").jump_next()
+				end,
+				desc = "Next TODO comment",
+			},
+			{
+				"[t",
+				function()
+					require("todo-comments").jump_prev()
+				end,
+				desc = "Previous TODO comment",
+			},
 		},
 		config = true,
 	},
