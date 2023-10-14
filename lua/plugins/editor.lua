135d9a5e2d1b5748c61f622757dbf01078432b60
Linnovs Liu <fforwm_cynnwys@slmail.me>
2023-10-14 23:56:36 +0800

    feat(flash): use flash instead of flit.nvim

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -232,12 +232,19 @@ return {
 	},
 
 	{
-		"ggandor/flit.nvim",
+		"folke/flash.nvim",
 		event = "BufReadPre",
-		dependencies = {
-			{ "ggandor/leap.nvim" },
-			{ "tpope/vim-repeat", version = false },
+		opts = {},
+        -- stylua: ignore
+		keys = {
+			{ "s", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash", },
+			{ "S", mode = { "n", "o", "x" }, function() require("flash").treesitter() end, desc = "Flash Treesitter", },
+			{ "r", mode = "o", function() require("flash").remote() end, desc = "Remote Flash", },
+			{ "R", mode = { "o", "x" }, function() require("flash").treesitter_search() end, desc = "Treesitter Search", },
+			{ "<c-s>", mode = { "c" }, function() require("flash").toggle() end, desc = "Toggle Flash Search", },
 		},
+	},
+
 		opts = {
 			labeled_modes = "nvo",
 		},
