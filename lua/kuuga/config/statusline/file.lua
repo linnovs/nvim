90004295c18ee2f1beff9751d1ac9bba6c49ff76
Linnovs Liu <fforwm_cynnwys@slmail.me>
2023-11-09 13:59:24 +0800

    feat(statusline): change file location fg bg color

--- lua/kuuga/config/statusline/file.lua
+++ lua/kuuga/config/statusline/file.lua
@@ -1,8 +1,8 @@
 local M = {}
 
 M.icon = { "filetype", icon_only = true, padding = { left = 1, right = 0 } }
-M.location = { "location" }
 M.name = { "filename", symbols = { modified = " ✏️ ", readonly = " 🔒" } }
+M.location = { "location", color = { fg = "#24273a", bg = "#b7bdf8" } }
 M.encoding = { "encoding", separator = "" }
 M.format = { "fileformat", symbols = { unix = " ", dos = " ", mac = " " }, separator = "" }
 M.type = { "filetype", separator = "" }
