7ddd630c331c2ab4c9698117318bb679837b35a8
Linnovs Liu <fforwm_cynnwys@slmail.me>
2023-11-20 03:30:00 +0800

    feat(mini_pairs): enable mapping only insert_mode/terminal

--- lua/plugins/coding.lua
+++ lua/plugins/coding.lua
@@ -63,7 +63,22 @@ return {
 	{
 		"echasnovski/mini.pairs",
 		events = "BufReadPost",
-		opts = {},
+		opts = {
+			modes = { insert = true, command = false, terminal = true },
+			mappings = {
+				["("] = { action = "open", pair = "()", neigh_pattern = "[^\\]." },
+				["["] = { action = "open", pair = "[]", neigh_pattern = "[^\\]." },
+				["{"] = { action = "open", pair = "{}", neigh_pattern = "[^\\]." },
+
+				[")"] = { action = "close", pair = "()", neigh_pattern = "[^\\]." },
+				["]"] = { action = "close", pair = "[]", neigh_pattern = "[^\\]." },
+				["}"] = { action = "close", pair = "{}", neigh_pattern = "[^\\]." },
+
+				['"'] = { action = "closeopen", pair = '""', neigh_pattern = "[^\\].", register = { cr = false } },
+				["'"] = { action = "closeopen", pair = "''", neigh_pattern = "[^%a\\].", register = { cr = false } },
+				["`"] = { action = "closeopen", pair = "``", neigh_pattern = "[^\\].", register = { cr = false } },
+			},
+		},
 	},
 
 	-- copilot
