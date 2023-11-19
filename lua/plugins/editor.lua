7c4d40b8897161c0eb28e9f7bf9f98c4e9ad4fe3
Linnovs Liu <fforwm_cynnwys@slmail.me>
2023-11-20 03:23:51 +0800

    feat(flash): add jump labels for ftFT

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -168,7 +168,13 @@ return {
 	{
 		"folke/flash.nvim",
 		event = "BufReadPre",
-		opts = {},
+		opts = {
+			modes = {
+				char = {
+					jump_labels = true,
+				},
+			},
+		},
         -- stylua: ignore
 		keys = {
 			{ "s", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash", },
