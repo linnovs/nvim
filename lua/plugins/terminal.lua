4e6ff0c645c922c61b18e46cd9f5ecd46823de54
Linnovs Liu <fforwm_cynnwys@slmail.me>
2023-11-21 20:34:07 +0800

    feat(plugin): use toggleterm instead of FTerm

--- lua/plugins/terminal.lua
+++ lua/plugins/terminal.lua
@@ -1,22 +1,29 @@
 return {
 	{
-		"numtostr/FTerm.nvim",
+		"akinsho/nvim-toggleterm.lua",
+		version = "*",
 		event = "VeryLazy",
-		config = function()
-			local fterm = require("FTerm")
-
-			fterm.setup({
-				border = "single",
-			})
-
-			-- local lg = fterm:new({
-			-- 	cmd = "lazygit",
-			-- })
-
-			vim.keymap.set({ "n", "t" }, "<Leader>tt", fterm.toggle, { desc = "Toggle Terminal" })
-			-- vim.keymap.set("n", "<Leader>gst", function()
-			-- 	lg:toggle()
-			-- end, { desc = "Toggle lazygit" })
-		end,
+		opts = {
+			size = function(term)
+				if term.direction == "horizontal" then
+					return 15
+				elseif term.direction == "vertical" then
+					return vim.o.columns * 0.4
+				end
+			end,
+			open_mapping = [[<Leader>tt]],
+			start_in_insert = true,
+			insert_mappings = false,
+			terminal_mappings = true,
+			persist_size = true,
+			persist_mode = true,
+			direction = "horizontal",
+			close_on_exit = true,
+			shell = vim.o.shell,
+			auto_scroll = true,
+			winbar = {
+				enabled = true,
+			},
+		},
 	},
 }
