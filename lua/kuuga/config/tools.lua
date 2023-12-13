ae4b5749549678043e6f445df8e7025468b71eaa
Linnovs Liu <fforwm_cynnwys@slmail.me>
2023-12-13 18:58:35 +0800

    feat(tools): linter, formatter, debugger all in one

---
+++ lua/kuuga/config/tools.lua
@@ -0,0 +1,37 @@
+local M = {}
+
+M.formatters = {
+	lua = { "stylua" },
+	go = { "gofmt", "goimports", "golines" },
+	zsh = { "beautysh" },
+	bash = { "beautysh" },
+	sh = { "beautysh" },
+	["*"] = { "trim_whitespace" },
+}
+
+M.linters = {}
+
+M.debugger = {}
+
+local pseudo_tools = {
+	"gofmt", -- gofmt is from golang.org/x/tools/cmd/gofmt
+	"trim_whitespace",
+}
+
+M.to_install = function()
+	local formatters = vim.tbl_flatten(vim.tbl_values(M.formatters))
+	local linters = vim.tbl_flatten(vim.tbl_values(M.linters))
+	local tools = vim.list_extend(vim.fn.copy(formatters), linters)
+	vim.list_extend(tools, M.debugger)
+
+	table.sort(tools)
+	tools = vim.fn.uniq(tools)
+
+	tools = vim.tbl_filter(function(tool)
+		return not vim.tbl_contains(pseudo_tools, tool)
+	end, vim.fn.copy(tools))
+
+	return tools
+end
+
+return M
