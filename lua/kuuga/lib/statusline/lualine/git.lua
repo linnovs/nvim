599f8f69135f6130554d22aabf76c8462e938171
Linnovs Liu <fforwm_cynnwys@slmail.me>
2024-01-12 10:18:42 +0800

    chore(lualine): remove old config

--- lua/kuuga/lib/statusline/lualine/git.lua
+++
@@ -1,6 +0,0 @@
-local M = {}
-
-M.branch = { "branch", color = { fg = "#ff9e64" } }
-M.diff = { "diff", symbols = { added = " ", modified = " ", removed = " " } }
-
-return M
--- lua/kuuga/lib/statusline/lualine/updates.lua
+++
@@ -1,8 +0,0 @@
-local lazyStatus = require("lazy.status")
-local M = {}
-
-M[1] = lazyStatus.updates
-M.cond = lazyStatus.has_updates
-M.color = { fg = "#ff9e64" }
-
-return M
