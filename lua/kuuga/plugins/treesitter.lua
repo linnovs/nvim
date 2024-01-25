b1dcd483619eb15e6e99b09829b3021aa8a127af
Linnovs Liu <fforwm_cynnwys@slmail.me>
2024-01-25 15:20:33 +0800

    feat(treesitter): disable TS when file is bigger than 100 KB

--- lua/kuuga/plugins/treesitter.lua
+++ lua/kuuga/plugins/treesitter.lua
@@ -58,7 +58,17 @@ return {
 				"yaml",
 				"zig",
 			},
-			highlight = { enable = true, additional_vim_regex_highlighting = false },
+			highlight = {
+				enable = true,
+				additional_vim_regex_highlighting = false,
+				disable = function(_, buf)
+					local max_filesize = 100 * 1024 -- 100 KB
+					local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
+					if ok and stats and stats.size > max_filesize then
+						return true
+					end
+				end,
+			},
 			indent = { enable = true },
 			textobjects = {
 				select = {
