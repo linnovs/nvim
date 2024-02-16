95c946285ae09149c2a2c48a4e3819b88b50e0d5
Linnovs Liu <fforwm_cynnwys@slmail.me>
2024-02-16 17:28:00 +0800

    feat(statuscol): use statusol.nvim instead

---
+++ lua/kuuga/plugins/statuscol.lua
@@ -0,0 +1,25 @@
+return {
+	"luukvbaal/statuscol.nvim",
+	opts = function()
+		local builtin = require("statuscol.builtin")
+
+		return {
+			segments = {
+				{
+					text = { "%s" },
+					click = "v:lua.ScSa",
+				},
+				{
+					text = { builtin.lnumfunc, " " },
+					condition = { true, builtin.not_empty },
+					click = "v:lua.ScLa",
+				},
+				{
+					text = { builtin.foldfunc, " " },
+					condition = { true, builtin.not_empty },
+					click = "v:lua:ScFa",
+				},
+			},
+		}
+	end,
+}
