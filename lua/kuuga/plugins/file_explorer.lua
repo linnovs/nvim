e3f5d305795170f590f435089ce689cbcb8837dd
Linnovs Liu <fforwm_cynnwys@slmail.me>
2024-02-23 11:47:05 +0800

    feat(oil): use <leader>wv to select vsplit

--- lua/kuuga/plugins/file_explorer.lua
+++ lua/kuuga/plugins/file_explorer.lua
@@ -4,6 +4,10 @@ return {
 	keys = {
 		{ "<Leader>of", "<CMD>Oil<CR>", desc = "Open oil (file explorer)" },
 	},
-	opts = {},
+	opts = {
+		keymaps = {
+			["<leader>wv"] = "actions.select_vsplit",
+		},
+	},
 	dependencies = { "nvim-tree/nvim-web-devicons" },
 }
