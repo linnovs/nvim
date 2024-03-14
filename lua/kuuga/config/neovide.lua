3516c6020e3d42679095eeb3d4778c8c89ef1cf2
Linnovs Liu <fforwm_cynnwys@slmail.me>
2024-03-15 00:29:49 +0800

    feat(neovide): disable cursor animation

--- lua/kuuga/config/neovide.lua
+++ lua/kuuga/config/neovide.lua
@@ -2,3 +2,4 @@ vim.g.neovide_transparency = 0.75
 vim.g.neovide_theme = "auto"
 vim.g.neovide_hide_mouse_when_typing = true
 vim.g.neovide_cursor_vfx_mode = "ripple"
+vim.g.neovide_cursor_animation_length = 0.00
