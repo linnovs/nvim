7ad824a16a98109a658473176276b233519361b0
Linnovs Liu <fforwm_cynnwys@slmail.me>
2024-04-05 21:09:22 +0800

    feat(plugins): use nvim-highlight-colors instead for colorizer

--- lua/kuuga/plugins/custom_highlights.lua
+++ lua/kuuga/plugins/custom_highlights.lua
@@ -25,28 +25,8 @@ return {
 	},
 
 	{
-		"NvChad/nvim-colorizer.lua",
+		"brenoprata10/nvim-highlight-colors",
 		event = "BufNew",
-		opts = {
-			filetypes = {
-				"*",
-				"!lazy",
-				"!gitcommit",
-				"!NeogitCommitMessage",
-			},
-			user_default_options = {
-				RGB = true, -- #RGB hex codes
-				RRGGBB = true, -- #RRGGBB hex codes
-				names = true, -- "Name" codes like Blue or blue
-				RRGGBBAA = true, -- #RRGGBBAA hex codes
-				AARRGGBB = true, -- 0xAARRGGBB hex codes
-				rgb_fn = true, -- CSS rgb() and rgba() functions
-				hsl_fn = true, -- CSS hsl() and hsla() functions
-				mode = "background", -- Set the display mode.
-				tailwind = true, -- Enable tailwind colors
-				virtualtext = "■",
-				always_update = false,
-			},
-		},
+		opts = true,
 	},
 }
