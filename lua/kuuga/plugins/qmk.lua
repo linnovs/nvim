369cc40deca5a6e805129b395f4b8270e342c9ca
Linnovs Liu <fforwm_cynnwys@slmail.me>
2024-04-15 21:48:27 +0800

    feat(plugin): add qmk

---
+++ lua/kuuga/plugins/qmk.lua
@@ -0,0 +1,16 @@
+return {
+	"codethread/qmk.nvim",
+	version = false,
+	opts = {
+		name = "LAYOUT",
+		comment_preview = {},
+		layout = {
+			"x x x x x x x _ _ _ _ _ x x x x x x x",
+			"x x x x x x x _ _ _ _ _ x x x x x x x",
+			"x x x x x x x _ _ _ _ _ x x x x x x x",
+			"x x x x x x _ _ _ _ _ _ _ x x x x x x",
+			"x x x x x _ x^x _ _ _ x^x _ x x x x x",
+			"_ _ _ _ _ _ x x x _ x x x _ _ _ _ _ _",
+		},
+	},
+}
