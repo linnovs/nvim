1ab8699a461044046797e0535e871f665b7dcb61
Linnovs Liu <fforwm_cynnwys@slmail.me>
2024-05-25 01:32:46 +0800

    feat(dap): setup mappings
    - F5: continue
    - F10: step over
    - F11: step into
    - F12: step oup
    - <LEADER>dtb: toggle breakpoint
    - <LEADER>dl: run last

--- lua/kuuga/plugins/dap.lua
+++ lua/kuuga/plugins/dap.lua
@@ -2,11 +2,58 @@ return {
 	{
 		"mfussenegger/nvim-dap",
 		lazy = true,
+		keys = {
+			{
+				"<F5>",
+				function()
+					require("dap").continue()
+				end,
+				desc = "Start/Continue DAP",
+			},
+			{
+				"<F10>",
+				function()
+					require("dap").step_over()
+				end,
+				desc = "Step over DAP",
+			},
+			{
+				"<F11>",
+				function()
+					require("dap").step_into()
+				end,
+				desc = "Step into DAP",
+			},
+			{
+				"<F12>",
+				function()
+					require("dap").step_out()
+				end,
+				desc = "Step out DAP",
+			},
+			{
+				"<leader>dtb",
+				function()
+					require("dap").toggle_breakpoint()
+				end,
+				desc = "Toggle breakpoint DAP",
+			},
+			{
+				"<leader>dl",
+				function()
+					require("dap").run_last()
+				end,
+				desc = "Run last DAP",
+			},
+		},
+		config = function()
+			vim.fn.sign_define("DapBreakpoint", { text = "🛑", texthl = "", linehl = "", numhl = "" })
+		end,
 	},
 
 	{
 		"rcarriga/nvim-dap-ui",
-		dependencies = { "mfussenegger/nvim-dap" },
+		dependencies = { "mfussenegger/nvim-dap", "nvim-neotest/nvim-nio" },
 		keys = {
 			{
 				"<leader>dap",
