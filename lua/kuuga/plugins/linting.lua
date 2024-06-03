3c089b1f1bdde09ec0a6ac32de7bc363037b6670
Linnovs Liu <fforwm_cynnwys@slmail.me>
2024-06-03 13:44:12 +0800

    fix(nvim-lint): use try_lint without debounce now

--- lua/kuuga/plugins/linting.lua
+++ lua/kuuga/plugins/linting.lua
@@ -4,35 +4,14 @@ return {
 	"mfussenegger/nvim-lint",
 	event = "BufNew",
 	config = function()
-		local M = {}
 		local lint = require("lint")
 		lint.linters_by_ft = linters
 
-		function M.debounce(ms, fn)
-			local timer = vim.loop.new_timer()
-			if timer == nil then
-				return function() end
-			end
-			return function(...)
-				local argv = { ... }
-				timer:start(ms, 0, function()
-					timer:stop()
-					vim.schedule_wrap(fn)(unpack(argv))
-				end)
-			end
-		end
-
-		function M.lint()
-			local names = lint.linters_by_ft[vim.bo.filetype] or {}
-
-			if #names > 0 then
-				lint.try_lint(names)
-			end
-		end
-
 		vim.api.nvim_create_autocmd({ "BufWritePost", "BufReadPost", "InsertLeave" }, {
 			group = vim.api.nvim_create_augroup("nvim-lint", { clear = true }),
-			callback = M.debounce(100, M.lint),
+			callback = function()
+				lint.try_lint()
+			end,
 		})
 	end,
 }
