8cb298a30f01392d6d5287c354827e76a1401b72
Linnovs Liu <fforwm_cynnwys@slmail.me>
2024-06-04 13:01:24 +0800

    fix(lsp): workaround for neovim/neovim#12970

--- lua/kuuga/lib/lsp.lua
+++ lua/kuuga/lib/lsp.lua
@@ -11,6 +11,18 @@ M.setup = function()
 	keymap("n", "<leader>d", vim.diagnostic.open_float, "Show diagnostic floating window")
 	keymap("n", "<leader>q", vim.diagnostic.setloclist, "Add buffer diagnostics to location list")
 
+	-- fixes for bug https://github.com/neovim/neovim/issues/12970
+	---@diagnostic disable-next-line: duplicate-set-field
+	vim.lsp.util.apply_text_document_edit = function(text_document_edit, _, offset_encoding)
+		local text_document = text_document_edit.textDocument
+		local bufnr = vim.uri_to_bufnr(text_document.uri)
+		if offset_encoding == nil then
+			vim.notify_once("apply_text_document_edit must be called with valid offset encoding", vim.log.levels.WARN)
+		end
+
+		vim.lsp.util.apply_text_edits(text_document_edit.edits, bufnr, offset_encoding)
+	end
+
 	vim.api.nvim_create_autocmd("LspAttach", {
 		group = vim.api.nvim_create_augroup("KuugaLspConfig", { clear = false }),
 		callback = function(ev)
