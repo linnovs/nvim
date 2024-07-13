110e4c89170099fb2ef1ba75aeb9d545bb804f64
Linnovs Liu <fforwm_cynnwys@slmail.me>
2024-07-13 23:10:22 +0800

    feat(rest): remove rest.nvim

--- lua/kuuga/plugins/rest.lua
+++
@@ -1,9 +0,0 @@
-return {
-	"rest-nvim/rest.nvim",
-	dependencies = { "nvim-lua/plenary.nvim" },
-	ft = "http",
-	keys = {
-		{ "<leader>rr", "<Plug>RestNvim<CR>", desc = "Run request under cursor", ft = "http" },
-	},
-	opts = {},
-}
