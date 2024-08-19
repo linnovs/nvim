f92f22e2e271386258f3ffca7a5adf73f8c00a14
Linnovs Liu <fforwm_cynnwys@slmail.me>
2024-08-19 20:49:49 +0800

    feat(colorscheme): remove unused custom_highlights

--- lua/kuuga/plugins/colorscheme.lua
+++ lua/kuuga/plugins/colorscheme.lua
@@ -12,30 +12,12 @@ return {
 			},
 			transparent_background = not vim.g.neovide,
 			custom_highlights = function(colors)
-				local telescopePreviewBg = colors.base
-				if vim.g.neovide then
-					telescopePreviewBg = colors.crust
-				end
-
 				return {
-					TelescopeSelection = { fg = colors.text, bg = colors.surface0, bold = true },
-					TelescopePromptPrefix = { bg = colors.surface0 },
-					TelescopePromptNormal = { bg = colors.surface0 },
-					TelescopeResultsNormal = { bg = colors.mantle },
-					TelescopePreviewNormal = { bg = telescopePreviewBg },
-					TelescopePromptBorder = { bg = colors.surface0, fg = colors.surface0 },
-					TelescopeResultsBorder = { bg = colors.mantle, fg = colors.mantle },
-					TelescopePreviewBorder = { bg = telescopePreviewBg, fg = telescopePreviewBg },
-					TelescopePromptTitle = { bg = colors.mauve, fg = colors.mantle },
-					TelescopeResultsTitle = { fg = colors.mantle },
-					TelescopePreviewTitle = { bg = colors.sapphire, fg = colors.mantle },
 					DiagnosticUnderlineError = { undercurl = true },
 					DiagnosticUnderlineWarn = { undercurl = true },
 					DiagnosticUnderlineInfo = { undercurl = true },
 					DiagnosticUnderlineHint = { undercurl = true },
 					DiagnosticUnderlineOk = { undercurl = true, sp = "LightGreen" },
-					Pmenu = { fg = colors.text, bg = colors.base },
-					NormalFloat = { fg = colors.text, bg = colors.base },
 					DashboardHeader = { fg = colors.mauve },
 					DashboardDesc = { fg = colors.lavender },
 					DashboardKey = { fg = colors.green },
