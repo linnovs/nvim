c6ff21eca2517879865a7d4ae994d694350cf192
Linnovs Liu <fforwm_cynnwys@slmail.me>
2024-10-09 18:17:51 +0800

    feat(codeium): change to codeium.vim to ditch out cmp

--- lua/kuuga/plugins/ai.lua
+++ lua/kuuga/plugins/ai.lua
@@ -1,9 +1,27 @@
+vim.g.codeium_disable_bindings = 1
+vim.g.codeium_manual = true
+
 return {
-	"Exafunction/codeium.nvim",
-	dependencies = {
-		"nvim-lua/plenary.nvim",
-		"hrsh7th/nvim-cmp",
-	},
-	event = "InsertEnter",
-	opts = {},
+	"Exafunction/codeium.vim",
+	branch = "main",
+	event = "BufEnter",
+	config = function()
+		local map = require("kuuga.lib.keymap").map
+
+		map("i", "<Tab>", function()
+			return vim.fn["codeium#Accept"]()
+		end, "Accept codeium suggestion", { silent = true, expr = true })
+
+		map("i", "<C-x>", function()
+			return vim.fn["codeium#Clear"]()
+		end, "Clear codeium suggestion", { silent = true, expr = true })
+
+		map("i", "<C-n>", function()
+			return vim.fn["codeium#CycleOrComplete"]()
+		end, "Next codeium suggestion", { silent = true, expr = true })
+
+		map("i", "<C-p>", function()
+			return vim.fn["codeium#CycleCompletions"](-1)
+		end, "Previous codeium suggestion", { silent = true, expr = true })
+	end,
 }
