9645e940b067146f50c6eb92f3b3f67abfd8c361
Linnovs Liu <fforwm_cynnwys@slmail.me>
2024-10-10 13:32:39 +0800

    feat(oil): set as default file explorer

--- lua/kuuga/plugins/file_explorer.lua
+++ lua/kuuga/plugins/file_explorer.lua
@@ -1,14 +1,17 @@
 return {
 	"stevearc/oil.nvim",
 	event = "VeryLazy",
+	dependencies = { "nvim-tree/nvim-web-devicons", version = false },
 	keys = {
 		{ "<Leader>of", "<CMD>Oil<CR>", desc = "Open oil (file explorer)" },
 	},
+	---@module 'oil'
+	---@type oil.SetupOpts
 	opts = {
+		default_file_explorer = true,
 		keymaps = {
 			["<leader>wv"] = "actions.select_vsplit",
 			["<C-s>"] = false,
 		},
 	},
-	dependencies = { "nvim-tree/nvim-web-devicons", version = false },
 }
