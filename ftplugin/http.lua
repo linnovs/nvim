fc6e9f43801071da1381ee0ee612d67b1077cb9d
Linnovs Liu <fforwm_cynnwys@slmail.me>
2024-10-26 02:43:10 +0800

    feat(http): change keymap
    <CR> to run
    ] go to next request
    [ go to prev request

--- ftplugin/http.lua
+++ ftplugin/http.lua
@@ -1,5 +1,5 @@
 local keymap = require("kuuga.lib.keymap")
 
-keymap.map("n", "<C-j>", "<CMD>lua require('kulala').jump_prev()<CR>", { noremap = true, silent = true })
-keymap.map("n", "<C-k>", "<CMD>lua require('kulala').jump_next()<CR>", { noremap = true, silent = true })
-keymap.map("n", "<C-l>", "<CMD>lua require('kulala').run()<CR>", { noremap = true, silent = true })
+keymap.map("n", "[", "<CMD>lua require('kulala').jump_prev()<CR>", "Prev request", { noremap = true, silent = true })
+keymap.map("n", "]", "<CMD>lua require('kulala').jump_next()<CR>", "Next request", { noremap = true, silent = true })
+keymap.map("n", "<CR>", "<CMD>lua require('kulala').run()<CR>", "Run request", { noremap = true, silent = true })
