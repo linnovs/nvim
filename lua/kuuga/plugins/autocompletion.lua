9bc1f300b37821f7d1f917b27c584842126fa484
Linnovs Liu <fforwm_cynnwys@slmail.me>
2024-11-01 01:23:48 +0800

    feat(blink-cmp): update default keymap with <CR> for confirm

--- lua/kuuga/plugins/autocompletion.lua
+++ lua/kuuga/plugins/autocompletion.lua
@@ -8,7 +8,20 @@ return {
 	---@module 'blink.cmp.config'
 	---@type blink.cmp.Config
 	opts = {
-		keymap = "default",
+		keymap = {
+			["<C-space>"] = { "show", "show_documentation", "hide_documentation" },
+			["<C-e>"] = { "hide" },
+			["<CR>"] = { "select_and_accept", "fallback" },
+
+			["<C-p>"] = { "select_prev", "fallback" },
+			["<C-n>"] = { "select_next", "fallback" },
+
+			["<C-b>"] = { "scroll_documentation_up", "fallback" },
+			["<C-f>"] = { "scroll_documentation_down", "fallback" },
+
+			["<Tab>"] = { "snippet_forward", "fallback" },
+			["<S-Tab>"] = { "snippet_backward", "fallback" },
+		},
 		accept = {
 			auto_brackets = { enabled = true },
 		},
