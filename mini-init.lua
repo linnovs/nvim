9be934fcd1dc1d500ba1c8894efb9149cee02f3f
Linnovs Liu <fforwm_cynnwys@slmail.me>
2024-10-31 22:40:14 +0800

    feat: mini init for lazyn.nvim

---
+++ mini-init.lua
@@ -0,0 +1,16 @@
+local lazypath = "/tmp/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable",
+		lazypath,
+	})
+end
+vim.opt.rtp:prepend(lazypath)
+vim.opt.termguicolors = true
+vim.opt.list = true
+
+require("lazy").setup({}, { root = "/tmp/lazy" })
