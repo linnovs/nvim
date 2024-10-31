996277b5de9b4d85003449915927fcc375dcaef8
Linnovs Liu <fforwm_cynnwys@slmail.me>
2024-10-31 22:48:04 +0800

    feat(blink-cmp): draw my own menu

--- lua/kuuga/plugins/autocompletion.lua
+++ lua/kuuga/plugins/autocompletion.lua
@@ -17,8 +17,24 @@ return {
 		windows = {
 			autocomplete = {
 				border = "rounded",
-				draw = "reversed",
 				selection = "manual",
+				draw = function(ctx)
+					return {
+						" ",
+						{ ctx.kind_icon, ctx.icon_gap, hl_group = "BlinkCmpKind" .. ctx.kind },
+						" ",
+						{
+							ctx.label,
+							ctx.kind == "Snippet" and "~" or nil,
+							fill = true,
+							hl_group = ctx.deprecated and "BlinkCmpLabelDeprecated" or "BlinkCmpLabel",
+							max_width = 50,
+						},
+						" ",
+						{ ctx.kind, hl_group = "BlinkCmpKind" .. ctx.kind },
+						" ",
+					}
+				end,
 			},
 			documentation = {
 				border = "rounded",
