7027d549ee23b5808909f2a52f1ccfd7068db53e
Linnovs Liu <fforwm_cynnwys@slmail.me>
2024-11-05 19:19:32 +0800

    docs(noice): add type annotation for opts

--- lua/kuuga/plugins/ui.lua
+++ lua/kuuga/plugins/ui.lua
@@ -3,6 +3,8 @@ return {
 		"folke/noice.nvim",
 		event = "VeryLazy",
 		dependencies = { "MunifTanjim/nui.nvim" },
+		---@module "noice"
+		---@type NoiceConfig
 		opts = {
 			lsp = {
 				override = {
