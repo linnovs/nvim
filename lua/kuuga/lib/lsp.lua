ce367cafa5c9d6056c47297f659f1ba7953a6957
Linnovs Liu <fforwm_cynnwys@slmail.me>
2024-11-08 15:03:05 +0800

    feat(lsp): add return after error

--- lua/kuuga/lib/lsp.lua
+++ lua/kuuga/lib/lsp.lua
@@ -18,6 +18,7 @@ M.setup = function()
 		local bufnr = vim.uri_to_bufnr(text_document.uri)
 		if offset_encoding == nil then
 			vim.notify_once("apply_text_document_edit must be called with valid offset encoding", vim.log.levels.WARN)
+			return
 		end
 
 		vim.lsp.util.apply_text_edits(text_document_edit.edits, bufnr, offset_encoding)
