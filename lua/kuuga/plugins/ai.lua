12e27e8cf996b367951026f297bfe80722975a89
Linnovs Liu <fforwm_cynnwys@slmail.me>
2024-11-08 16:49:04 +0800

    fix(codeium): remove expr and silent for <C-x>,<C-n>,<C-p>

--- lua/kuuga/plugins/ai.lua
+++ lua/kuuga/plugins/ai.lua
@@ -14,14 +14,14 @@ return {
 
 		map("i", "<C-x>", function()
 			return vim.fn["codeium#Clear"]()
-		end, "Clear codeium suggestion", { silent = true, expr = true })
+		end, "Clear codeium suggestion")
 
 		map("i", "<C-n>", function()
 			return vim.fn["codeium#CycleOrComplete"]()
-		end, "Next codeium suggestion", { silent = true, expr = true })
+		end, "Next codeium suggestion")
 
 		map("i", "<C-p>", function()
 			return vim.fn["codeium#CycleCompletions"](-1)
-		end, "Previous codeium suggestion", { silent = true, expr = true })
+		end, "Previous codeium suggestion")
 	end,
 }
