ff0526cefaddfd474dea3b3c5da4a5707ecb1997
Linnovs Liu <fforwm_cynnwys@slmail.me>
2024-11-30 22:07:34 +0800

    feat(filetype): add pattern for github(gitea) action

--- lua/kuuga/config/filetypes.lua
+++ lua/kuuga/config/filetypes.lua
@@ -1,4 +1,9 @@
 vim.filetype.add({
+	pattern = {
+		[".*/.github/workflows/.*%.yml"] = "yaml.ghaction",
+		[".*/.github/workflows/.*%.yaml"] = "yaml.ghaction",
+		[".*/.gitea/workflows/.*%.yaml"] = "yaml.ghaction",
+	},
 	extension = {
 		hujson = "jsonc",
 		["http"] = "http",
