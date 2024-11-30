baa8f47bd734f8671f5b03353bb68536eae55511
Linnovs Liu <fforwm_cynnwys@slmail.me>
2024-11-30 17:46:42 +0800

    feat(clipboard): remove the use of system clipboard & add mapping to yank/paste to/from clipboard

--- lua/kuuga/config/mappings.lua
+++ lua/kuuga/config/mappings.lua
@@ -26,6 +26,15 @@ keymap.map("n", "<Leader>qq", "<cmd>quit<CR>", "Quick quit")
 keymap.map("n", "<Leader>qa", "<cmd>qall<CR>", "Quick quit all")
 keymap.map("n", "<Leader>fqq", "<cmd>quit!<CR>", "Quick force quit")
 
+-- clipboard management
+-- stylua: ignore start
+keymap.map({ "n", "v", "x" }, "<Leader>y", '"+y', "Yank to clipboard", { silent = true, noremap = true })
+keymap.map({ "n", "v", "x" }, "<Leader>yy", '"+yy', "Yank link to clipboard", { silent = true, noremap = true })
+keymap.map({ "n", "v", "x" }, "<Leader>Y", '"+Y', "Yank to clipboard", { silent = true, noremap = true })
+keymap.map({ "n", "v", "x" }, "<Leader>p", '"+p', "Paste from clipboard", { silent = true, noremap = true })
+keymap.map( "x", "<Leader>P", '"_dP', "Paste over selection without erasing unnamed register", { silent = true, noremap = true })
+-- stylua: ignore end
+
 keymap.map({ "i", "n" }, "<esc>", "<cmd>noh<cr><esc>", "Escape and clear hlsearch")
 
 keymap.map("n", "<Leader>lazy", "<CMD>Lazy<CR>", "Open Lazy.nvim")
