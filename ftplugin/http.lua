b1069f2d541ca31b8e5183c85553d9ffc428a362
Linnovs Liu <fforwm_cynnwys@slmail.me>
2024-12-21 02:38:50 +0800

    feat(http): add keymap to toggle kulala view (headers)

--- ftplugin/http.lua
+++ ftplugin/http.lua
@@ -1,5 +1,12 @@
 local keymap = require("kuuga.lib.keymap")
 
+keymap.map(
+	"n",
+	"<leader>tv",
+	"<CMD>lua require('kulala').toggle_view()<CR>",
+	"Toggle view",
+	{ noremap = true, silent = true }
+)
 keymap.map("n", "[", "<CMD>lua require('kulala').jump_prev()<CR>", "Prev request", { noremap = true, silent = true })
 keymap.map("n", "]", "<CMD>lua require('kulala').jump_next()<CR>", "Next request", { noremap = true, silent = true })
 keymap.map("n", "<CR>", "<CMD>lua require('kulala').run()<CR>", "Run request", { noremap = true, silent = true })
