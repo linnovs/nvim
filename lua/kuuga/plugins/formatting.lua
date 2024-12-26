c9b161a074f6aa13d420762867c15500432778e0
Linnovs Liu <fforwm_cynnwys@slmail.me>
2024-12-27 00:51:07 +0800

    feat(conform): add default config file to prettierd

--- lua/kuuga/plugins/formatting.lua
+++ lua/kuuga/plugins/formatting.lua
@@ -7,6 +7,8 @@ return {
 	init = function()
 		vim.o.formatexpr = "v:lua.require('conform').formatexpr()"
 	end,
+	---@module "conform"
+	---@type conform.setupOpts
 	opts = {
 		format_on_save = function(bufnr)
 			-- Disable with a global or buffer-local variable
@@ -16,6 +18,13 @@ return {
 			return { timeout_ms = 500, lsp_format = "fallback" }
 		end,
 		formatters_by_ft = formatters,
+		formatters = {
+			prettierd = {
+				env = {
+					PRETTIERD_DEFAULT_CONFIG = vim.fn.stdpath("config") .. "/.prettierrc.json",
+				},
+			},
+		},
 		notify_on_error = true,
 	},
 }
