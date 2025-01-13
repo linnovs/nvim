c9c7caa49e1434c634b7a8273303d434840f9805
Linnovs Liu <fforwm_cynnwys@slmail.me>
2025-01-13 20:24:03 +0800

    feat(quickfix): add quicker.nvim for better quickfix menu

--- lua/kuuga/plugins/better_quickfix.lua
+++ lua/kuuga/plugins/better_quickfix.lua
@@ -1,4 +1,14 @@
 return {
-	"kevinhwang91/nvim-bqf",
-	ft = "qf",
+	{
+		"kevinhwang91/nvim-bqf",
+		ft = "qf",
+	},
+
+	{
+		"stevearc/quicker.nvim",
+		ft = "qf",
+		---@module 'quicker'
+		---@type quicker.SetupOptions
+		opts = {},
+	},
 }
