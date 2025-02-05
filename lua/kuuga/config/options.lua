dc94ddc37b9acf4ba759785ce0af9726b3ff213f
Linnovs Liu <fforwm_cynnwys@slmail.me>
2025-02-05 15:13:05 +0800

    feat(options): add diffopt algorithm histogram & linematch to 50

--- lua/kuuga/config/options.lua
+++ lua/kuuga/config/options.lua
@@ -5,6 +5,7 @@ vim.opt.colorcolumn = "+0" -- columns to highlight
 vim.opt.completeopt = { "menu", "menuone", "preview", "noinsert", "noselect" } -- options for Insert mode completion
 vim.opt.conceallevel = 0 -- whether concealable text is shown or hidden
 vim.opt.cursorline = true -- highlight the screen line of the cursor
+vim.opt.diffopt:append({ algorithm = "histogram", linematch = 50 })
 vim.opt.expandtab = true -- use spaces when <Tab> is inserted
 vim.opt.fileencoding = "utf-8" -- file encoding for multibyte text
 vim.opt.fillchars = { eob = " ", fold = " ", foldopen = "", foldsep = " ", foldclose = "" } -- characters to use for displaying special items
