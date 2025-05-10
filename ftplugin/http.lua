7acd45d29c98205dae98a6916b17fd6a661806bf
Linnovs Liu <fforwm_cynnwys@slmail.me>
2025-05-10 10:22:06 +0800

    fix(http): keymap should be buffer only

--- ftplugin/http.lua
+++ ftplugin/http.lua
@@ -2,11 +2,22 @@ local keymap = require("kuuga.lib.keymap")
 
 keymap.map(
 	"n",
-	"<leader>tv",
-	"<CMD>lua require('kulala').toggle_view()<CR>",
-	"Toggle view",
-	{ noremap = true, silent = true }
+	"[",
+	"<CMD>lua require('kulala').jump_prev()<CR>",
+	"Prev request",
+	{ noremap = true, silent = true, buffer = true }
 )
-keymap.map("n", "[", "<CMD>lua require('kulala').jump_prev()<CR>", "Prev request", { noremap = true, silent = true })
-keymap.map("n", "]", "<CMD>lua require('kulala').jump_next()<CR>", "Next request", { noremap = true, silent = true })
-keymap.map("n", "<CR>", "<CMD>lua require('kulala').run()<CR>", "Run request", { noremap = true, silent = true })
+keymap.map(
+	"n",
+	"]",
+	"<CMD>lua require('kulala').jump_next()<CR>",
+	"Next request",
+	{ noremap = true, silent = true, buffer = true }
+)
+keymap.map(
+	"n",
+	"<CR>",
+	"<CMD>lua require('kulala').run()<CR>",
+	"Run request",
+	{ noremap = true, silent = true, buffer = true }
+)
