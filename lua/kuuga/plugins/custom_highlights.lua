6e97db18e79f86a63f80d92ee8a463fc23075d29
Linnovs Liu <fforwm_cynnwys@slmail.me>
2025-06-03 12:32:40 +0800

    fix(todo): change to use snacks picker

--- lua/kuuga/plugins/custom_highlights.lua
+++ lua/kuuga/plugins/custom_highlights.lua
@@ -3,22 +3,11 @@ return {
 		"folke/todo-comments.nvim",
 		cmd = { "TodoTrouble", "TodoFzfLua" },
 		event = "BufReadPost",
+		version = false,
 		keys = {
-			{ "<leader>ft", "<cmd>TodoFzfLua<CR>", desc = "Find TODOs" },
-			{
-				"]t",
-				function()
-					require("todo-comments").jump_next()
-				end,
-				desc = "Next TODO comment",
-			},
-			{
-				"[t",
-				function()
-					require("todo-comments").jump_prev()
-				end,
-				desc = "Previous TODO comment",
-			},
+			{ "<leader>ft", function() Snacks.picker.todo_comments() end, desc = "Find TODOs" },
+			{ "]t", function() require("todo-comments").jump_next() end, desc = "Next TODO comment" },
+			{ "[t", function() require("todo-comments").jump_prev() end, desc = "Previous TODO comment" },
 		},
 		config = true,
 	},
