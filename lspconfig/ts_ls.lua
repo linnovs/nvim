f18bfc3eb339703551a2c1e9d8846e6960eddd8b
Linnovs Liu <fforwm_cynnwys@slmail.me>
2025-06-30 06:29:30 +0800

    feat(lsp): add typescript/javascript inhay hint settings

--- lspconfig/ts_ls.lua
+++ lspconfig/ts_ls.lua
@@ -10,6 +10,33 @@ return {
 			},
 		},
 	},
+	settings = {
+		typescript = {
+			inlayHints = {
+				includeInlayParameterNameHints = "all", -- 'none' | 'literals' | 'all'
+				includeInlayParameterNameHintsWhenArgumentMatchesName = true,
+				includeInlayVariableTypeHints = true,
+				includeInlayFunctionParameterTypeHints = true,
+				includeInlayVariableTypeHintsWhenTypeMatchesName = true,
+				includeInlayPropertyDeclarationTypeHints = true,
+				includeInlayFunctionLikeReturnTypeHints = true,
+				includeInlayEnumMemberValueHints = true,
+			},
+		},
+		javascript = {
+			inlayHints = {
+				includeInlayParameterNameHints = "all", -- 'none' | 'literals' | 'all'
+				includeInlayParameterNameHintsWhenArgumentMatchesName = true,
+				includeInlayVariableTypeHints = true,
+
+				includeInlayFunctionParameterTypeHints = true,
+				includeInlayVariableTypeHintsWhenTypeMatchesName = true,
+				includeInlayPropertyDeclarationTypeHints = true,
+				includeInlayFunctionLikeReturnTypeHints = true,
+				includeInlayEnumMemberValueHints = true,
+			},
+		},
+	},
 	filetypes = {
 		"javascript",
 		"javascriptreact",
