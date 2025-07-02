83de43bb5dc6dac84717cbdd527d07d38a9e406f
Linnovs Liu <fforwm_cynnwys@slmail.me>
2025-07-02 23:42:47 +0800

    feat(typescript): add keymap for source code action

---
+++ ftplugin/typescript.lua
@@ -0,0 +1,3 @@
+local keymap = require("kuuga.lib.keymap")
+
+keymap.map("n", "<Leader>sca", "<CMD>LspTypescriptSourceAction<CR>", "Source code action")
---
+++ ftplugin/vue.lua
@@ -0,0 +1,1 @@
+/home/linnovs/.config/nvim/ftplugin/typescript.lua
