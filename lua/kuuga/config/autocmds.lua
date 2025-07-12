e5d125b79b67e3a58554ce7be3c514d317ebb627
Linnovs Liu <fforwm_cynnwys@slmail.me>
2025-07-13 01:58:15 +0800

    feat(tmux): run plugin rename window when vimenter/vimleave

--- lua/kuuga/config/autocmds.lua
+++ lua/kuuga/config/autocmds.lua
@@ -25,6 +25,22 @@ autocmd("BufWritePost", {
 	end,
 })
 
+autocmd({ "VimEnter", "VimLeave" }, {
+	callback = function()
+		if not vim.env.TMUX_PLUGIN_MANAGER_PATH then return end
+
+		local file = vim.env.TMUX_PLUGIN_MANAGER_PATH .. "/tmux-window-name/scripts/rename_session_windows.py"
+		if vim.fn.filereadable(file) then
+			vim.uv.spawn(
+				file,
+				---@diagnostic disable-next-line: missing-fields
+				{},
+				function() end
+			)
+		end
+	end,
+})
+
 autocmd("BufEnter", { pattern = { "term://*", "*COMMIT_EDITMSG" }, command = "startinsert" })
 autocmd({ "BufReadPost", "BufNewFile" }, { pattern = "*.tf", command = "set filetype=terraform" })
 autocmd({ "BufReadPost", "BufNewFile" }, { pattern = "*.rasi", command = "set filetype=rasi" })
