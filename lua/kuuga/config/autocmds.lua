5f6fc983f48bff45c85fd09eacfe0b16078286d1
Linnovs Liu <fforwm_cynnwys@slmail.me>
2025-07-31 01:05:04 +0800

    style(autocmd): remove extra line before/after vim.uv.spawn

--- lua/kuuga/config/autocmds.lua
+++ lua/kuuga/config/autocmds.lua
@@ -52,12 +52,8 @@ autocmd({ "VimEnter", "VimLeave" }, {
 
 		local file = vim.env.TMUX_PLUGIN_MANAGER_PATH .. "/tmux-window-name/scripts/rename_session_windows.py"
 		if vim.fn.filereadable(file) then
-			vim.uv.spawn(
-				file,
-				---@diagnostic disable-next-line: missing-fields
-				{},
-				function() end
-			)
+			---@diagnostic disable-next-line: missing-fields
+			vim.uv.spawn(file, {}, function() end)
 		end
 	end,
 })
