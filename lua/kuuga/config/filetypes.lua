0422a9df17471b258feb0432f2a4598579349444
Linnovs Liu <fforwm_cynnwys@slmail.me>
2025-08-09 23:15:40 +0800

    feat(filetype): add yaml.docker-compose when file named compose.ya?ml

--- lua/kuuga/config/filetypes.lua
+++ lua/kuuga/config/filetypes.lua
@@ -3,6 +3,7 @@ vim.filetype.add({
 		[".*/.github/workflows/.*%.yml"] = "yaml.ghaction",
 		[".*/.github/workflows/.*%.yaml"] = "yaml.ghaction",
 		[".*/.gitea/workflows/.*%.yaml"] = "yaml.ghaction",
+		[".*/compose.ya?ml"] = "yaml.docker-compose",
 	},
 	extension = {
 		hujson = "jsonc",
