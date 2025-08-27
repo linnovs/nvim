c6a6c92e448768e5ab1fdb541f569dd5ccb3d1ae
Linnovs Liu <no-reply@linnovs.com>
2025-08-28 01:54:16 +0800

    feat(flash): update style and add toggle notification in command mode

--- lua/kuuga/plugins/easymotion.lua
+++ lua/kuuga/plugins/easymotion.lua
@@ -9,40 +9,37 @@ return {
 		{
 			"s",
 			mode = { "n", "x", "o" },
-			function()
-				require("flash").jump()
-			end,
+			function() require("flash").jump() end,
 			desc = "Flash",
 		},
 		{
 			"S",
 			mode = { "n", "o", "x" },
-			function()
-				require("flash").treesitter()
-			end,
+			function() require("flash").treesitter() end,
 			desc = "Flash Treesitter",
 		},
 		{
 			"r",
 			mode = "o",
-			function()
-				require("flash").remote()
-			end,
+			function() require("flash").remote() end,
 			desc = "Remote Flash",
 		},
 		{
 			"R",
 			mode = { "o", "x" },
-			function()
-				require("flash").treesitter_search()
-			end,
+			function() require("flash").treesitter_search() end,
 			desc = "Treesitter Search",
 		},
 		{
 			"<c-s>",
 			mode = { "c" },
 			function()
-				require("flash").toggle()
+				local state = require("flash").toggle()
+				vim.notify(
+					"Toggle Flash Search " .. (state and "ON" or "OFF"),
+					vim.log.levels.INFO,
+					{ title = "Flash", id = "toggle-flash" }
+				)
 			end,
 			desc = "Toggle Flash Search",
 		},
