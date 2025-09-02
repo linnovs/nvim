53698656705fd20cf2b4ddb9c779ea7b2fefa4f0
Linnovs Liu <no-reply@linnovs.com>
2025-09-02 17:00:43 +0800

    chore(image): remove usused config for codecompanion

--- lua/kuuga/plugins/image.lua
+++ lua/kuuga/plugins/image.lua
@@ -2,14 +2,6 @@ return {
 	{
 		"HakonHarnes/img-clip.nvim",
 		event = "VeryLazy",
-		opts = {
-			filetypes = {
-				codecompanion = {
-					prompt_for_file_name = false,
-					template = "[Image]($FILE_PATH)",
-					use_absolute_path = true,
-				},
-			},
-		},
+		opts = {},
 	},
 }
