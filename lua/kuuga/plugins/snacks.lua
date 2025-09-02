74700b432c3e4313ebac059a0e110754ea910422
Linnovs Liu <no-reply@linnovs.com>
2025-09-02 17:00:58 +0800

    feat(snacks): add image resolve function for obsidian

--- lua/kuuga/plugins/snacks.lua
+++ lua/kuuga/plugins/snacks.lua
@@ -10,7 +10,15 @@ return {
 		opts = {
 			bigfile = { enabled = true },
 			indent = { enabled = true, chunk = { enabled = true } },
-			image = { enabled = true },
+			image = {
+				enabled = true,
+				resolve = function(path, src)
+					if require("obsidian.api").path_is_note(path) then
+						return require("obsidian.api").resolve_image_path(src)
+					end
+					return nil
+				end,
+			},
 			notifier = { enabled = true },
 			quickfile = { enabled = true },
 			scroll = { enabled = true },
