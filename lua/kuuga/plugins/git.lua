016e60da84ceb6d6dc08ddec3fbca65cb4cabd9b
Linnovs Liu <no-reply@linnovs.com>
2025-09-09 23:21:12 +0800

    feat(gitsignt): update staged_signs

--- lua/kuuga/plugins/git.lua
+++ lua/kuuga/plugins/git.lua
@@ -11,6 +11,14 @@ return {
 				changedelete = { text = "▎" },
 				untracked = { text = "▎" },
 			},
+			signs_staged = {
+				add = { text = "▎" },
+				change = { text = "▎" },
+				delete = { text = "_" },
+				topdelete = { text = "‾" },
+				changedelete = { text = "▎" },
+				untracked = { text = "▎" },
+			},
 			current_line_blame = true,
 			on_attach = function()
 				local gs = package.loaded["gitsigns"]
