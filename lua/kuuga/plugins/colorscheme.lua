7be17530482ed50ae5095c9feda7ffa6da2e2764
Linnovs Liu <no-reply@linnovs.com>
2025-09-10 07:41:23 +0800

    feat(catppuccin): remove unnecessary function for opts

--- lua/kuuga/plugins/colorscheme.lua
+++ lua/kuuga/plugins/colorscheme.lua
@@ -4,30 +4,28 @@ return {
 	priority = 1000,
 	name = "catppuccin",
 	version = false,
-	opts = function()
-		---@module 'catppuccin.types'
-		---@type CatppuccinOptions
-		return {
-			flavor = "mocha",
-			background = {
-				light = "latte",
-				dark = "mocha",
-			},
-			transparent_background = true,
-			show_end_of_buffer = true,
-			custom_highlights = function(colors)
-				return {
-					DiagnosticUnderlineError = { undercurl = true },
-					DiagnosticUnderlineWarn = { undercurl = true },
-					DiagnosticUnderlineInfo = { undercurl = true },
-					DiagnosticUnderlineHint = { undercurl = true },
-					DiagnosticUnderlineOk = { undercurl = true, sp = "LightGreen" },
-					LspFloatWinNormal = { fg = colors.text, bg = colors.base },
-					DashboardHeader = { fg = colors.mauve },
-				}
-			end,
-			default_integrations = false,
-			auto_integrations = true,
-		}
-	end,
+	---@module 'catppuccin.types'
+	---@type CatppuccinOptions
+	opts = {
+		flavor = "mocha",
+		background = {
+			light = "latte",
+			dark = "mocha",
+		},
+		transparent_background = true,
+		show_end_of_buffer = true,
+		custom_highlights = function(colors)
+			return {
+				DiagnosticUnderlineError = { undercurl = true },
+				DiagnosticUnderlineWarn = { undercurl = true },
+				DiagnosticUnderlineInfo = { undercurl = true },
+				DiagnosticUnderlineHint = { undercurl = true },
+				DiagnosticUnderlineOk = { undercurl = true, sp = "LightGreen" },
+				LspFloatWinNormal = { fg = colors.text, bg = colors.base },
+				DashboardHeader = { fg = colors.mauve },
+			}
+		end,
+		default_integrations = false,
+		auto_integrations = true,
+	},
 }
