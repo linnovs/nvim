b3e4bfa1e73f6471f619e04eab1c59db1c29a4b2
Linnovs Liu <no-reply@linnovs.com>
2025-09-27 22:46:00 +0800

    feat(autocmd): amend last commit if is chore(lazy) update lazy-lock.json after update

--- lua/kuuga/config/autocmds.lua
+++ lua/kuuga/config/autocmds.lua
@@ -76,28 +76,72 @@ autocmd("User", {
 	pattern = "LazyUpdate",
 	callback = function()
 		vim.defer_fn(function()
+			local commitMesg = "chore(lazy): update lazy-lock.json"
 			uv.spawn("git", {
 				cwd = vim.fn.stdpath("config"),
 				args = { "add", "lazy-lock.json" },
 			}, function(addCode)
-				if addCode == 0 then
-					uv.spawn("git", {
-						cwd = vim.fn.stdpath("config"),
-						args = { "commit", "-m", "chore(lazy): update lazy-lock.json" },
-					}, function(commitCode)
-						if commitCode == 0 then
-							vim.notify("Committed lazy-lock.json", vim.log.levels.INFO, { title = "Lazy" })
-						else
-							vim.notify(
-								"Failed to commit lazy-lock.json: " .. commitCode,
-								vim.log.levels.ERROR,
-								{ title = "Lazy" }
-							)
-						end
-					end)
-				else
+				if addCode ~= 0 then
 					vim.notify("Failed to add lazy-lock.json: " .. addCode, vim.log.levels.ERROR, { title = "Lazy" })
+					return
 				end
+
+				local pipe = uv.new_pipe()
+
+				uv.spawn("git", {
+					cwd = vim.fn.stdpath("config"),
+					args = { "log", "-1", "--pretty=format:%B" },
+					stdio = { nil, pipe, nil },
+				}, function(logCode)
+					if logCode ~= 0 then
+						vim.notify(
+							"Failed to get last commit message: " .. logCode,
+							vim.log.levels.ERROR,
+							{ title = "Lazy" }
+						)
+						return
+					end
+
+					uv.spawn("grep", {
+						args = { "-q", commitMesg },
+						stdio = { pipe, nil, nil },
+					}, function(grepCode)
+						if grepCode == 0 then
+							-- amend the last commit
+							uv.spawn("git", {
+								cwd = vim.fn.stdpath("config"),
+								args = { "commit", "--amend", "--no-edit" },
+							}, function(amendCode)
+								if amendCode == 0 then
+									vim.notify("Amended last chore commit", vim.log.levels.INFO, { title = "Lazy" })
+								else
+									vim.notify(
+										"Failed to amend lazy-lock.json: " .. amendCode,
+										vim.log.levels.ERROR,
+										{ title = "Lazy" }
+									)
+								end
+							end)
+
+							return
+						end
+
+						uv.spawn("git", {
+							cwd = vim.fn.stdpath("config"),
+							args = { "commit", "-m", commitMesg },
+						}, function(commitCode)
+							if commitCode == 0 then
+								vim.notify("Committed lazy-lock.json", vim.log.levels.INFO, { title = "Lazy" })
+							else
+								vim.notify(
+									"Failed to commit lazy-lock.json: " .. commitCode,
+									vim.log.levels.ERROR,
+									{ title = "Lazy" }
+								)
+							end
+						end)
+					end)
+				end)
 			end)
 		end, 300)
 	end,
