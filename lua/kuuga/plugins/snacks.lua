af56314d5d9edb4cc6032b8b0f42db8f253445f8
Linnovs Liu <no-reply@linnovs.com>
2026-01-16 13:01:06 +0800

    feat(snacks): notify lsp after oil rename file

--- lua/kuuga/plugins/snacks.lua
+++ lua/kuuga/plugins/snacks.lua
@@ -172,6 +172,15 @@ return {
 					Snacks.toggle.inlay_hints():map("yoih")
 				end,
 			})
+
+			vim.api.nvim_create_autocmd("User", {
+				pattern = "OilActionsPost",
+				callback = function(event)
+					if event.data.actions[1].type == "move" then
+						Snacks.rename.on_rename_file(event.data.actions[1].src_url, event.data.actions[1].dest_url)
+					end
+				end,
+			})
 		end,
 	},
 }
