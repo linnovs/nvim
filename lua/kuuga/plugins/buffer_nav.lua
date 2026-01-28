2f2920d00321ce5c91dfced84b5edda87876fcab
Linnovs Liu <no-reply@linnovs.com>
2026-01-29 02:02:18 +0800

    feat(buffer_nav): update jump labels for bafa.nvim

--- lua/kuuga/plugins/buffer_nav.lua
+++ lua/kuuga/plugins/buffer_nav.lua
@@ -12,6 +12,20 @@ return {
 				desc = "Buffer navigation with bafa.nvim",
 			},
 		},
-		opts = {},
+		---@module 'bafa.Config'
+		---@type BafaUserConfig
+		opts = {
+			ui = {
+				jump_labels = {
+					-- stylua: ignore start
+					keys = {
+						"a", "r", "s", "t", "n", "e", "i", "o", -- home row
+						"q", "w", "f", "p", "l", "u", "y", ";", -- top row
+						"z", "x", "c", "k", "h", ",", ".",      -- bottom row
+					},
+					-- stylua: ignore end
+				},
+			},
+		},
 	},
 }
