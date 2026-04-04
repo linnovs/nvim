1b45a52dd3fb31f42ca2aa017e67e30ce82b3fb0
Linnovs Liu <no-reply@linnovs.com>
2026-04-05 04:14:37 +0800

    feat(plugin): add completion (blink.cmp)

---
+++ plugin/30-dependencies.lua
@@ -0,0 +1,4 @@
+vim.pack.add({
+	"https://github.com/brenoprata10/nvim-highlight-colors",
+	"https://github.com/nvim-tree/nvim-web-devicons",
+})
---
+++ plugin/completion.lua
@@ -0,0 +1,107 @@
+vim.schedule(function()
+	vim.pack.add({
+		"https://github.com/MahanRahmati/blink-nerdfont.nvim",
+		{ src = "https://github.com/fang2hou/blink-copilot", version = vim.version.range("*") },
+		"https://github.com/rafamadriz/friendly-snippets",
+		"https://github.com/xzbdmw/colorful-menu.nvim",
+		{ src = "https://github.com/saghen/blink.cmp", version = vim.version.range("*") },
+	})
+
+	require("blink.cmp").setup({
+		keymap = {
+			preset = "enter",
+			["<Tab>"] = {
+				"snippet_forward",
+				function()
+					require("sidekick").nes_jump_or_apply()
+				end,
+				"fallback",
+			},
+		},
+		sources = {
+			default = { "lsp", "path", "snippets", "buffer", "nerdfont", "copilot" },
+			per_filetype = {
+				lua = { "lsp", "path", "snippets", "buffer", "lazydev", "nerdfont", "copilot" },
+				sql = { "lsp", "snippets", "buffer", "dadbob", "copilot" },
+				psql = { "lsp", "snippets", "buffer", "dadbob", "copilot" },
+				mysql = { "lsp", "snippets", "buffer", "dadbob", "copilot" },
+				oil = { "lsp", "path", "buffer", "copilot" },
+			},
+			providers = {
+				copilot = {
+					name = "Copilot",
+					module = "blink-copilot",
+					score_offset = 100,
+					async = true,
+				},
+				nerdfont = {
+					name = "NerdFont",
+					module = "blink-nerdfont",
+					score_offset = 15,
+					opts = { insert = true },
+				},
+				lazydev = { name = "LazyDev", module = "lazydev.integrations.blink", fallbacks = { "lsp" } },
+				dadbod = { name = "Dadbod", module = "vim_dadbod_completion.blink" },
+			},
+		},
+		completion = {
+			list = { selection = { preselect = false, auto_insert = false } },
+			documentation = {
+				auto_show = false,
+				auto_show_delay_ms = 200,
+				window = {
+					border = "rounded",
+					winblend = 30,
+				},
+			},
+			menu = {
+				winblend = 30,
+				draw = {
+					columns = { { "kind_icon" }, { "label", gap = 1 }, { "source_name" } },
+					components = {
+						kind_icon = {
+							text = function(ctx)
+								-- default kind icon
+								local icon = ctx.kind_icon
+								-- if LSP source, check for color derived from documentation
+								if ctx.item.source_name == "LSP" then
+									local color_item = require("nvim-highlight-colors").format(
+										ctx.item.documentation,
+										{ kind = ctx.kind }
+									)
+									if color_item and color_item.abbr ~= "" then
+										icon = color_item.abbr
+									end
+								end
+								return icon .. ctx.icon_gap
+							end,
+							highlight = function(ctx)
+								-- default highlight group
+								local highlight = "BlinkCmpKind" .. ctx.kind
+								-- if LSP source, check for color derived from documentation
+								if ctx.item.source_name == "LSP" then
+									local color_item = require("nvim-highlight-colors").format(
+										ctx.item.documentation,
+										{ kind = ctx.kind }
+									)
+									if color_item and color_item.abbr_hl_group then
+										highlight = color_item.abbr_hl_group
+									end
+								end
+								return highlight
+							end,
+						},
+						label = {
+							text = function(ctx)
+								return require("colorful-menu").blink_components_text(ctx)
+							end,
+							highlight = function(ctx)
+								return require("colorful-menu").blink_components_highlight(ctx)
+							end,
+						},
+					},
+				},
+			},
+		},
+	})
+end)
