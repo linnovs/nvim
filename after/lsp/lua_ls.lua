9b1d6f6ae93bb247d2641482edc5f0325e84557a
Linnovs Liu <no-reply@linnovs.com>
2026-04-05 07:51:21 +0800

    feat(lsp): setup lua_ls

---
+++ after/lsp/lua_ls.lua
@@ -0,0 +1,18 @@
+return {
+	settings = {
+		Lua = {
+			hint = {
+				enable = true,
+			},
+			format = {
+				enable = false,
+			},
+			workspace = {
+				checkThirdParty = false,
+			},
+			completion = {
+				callSnippet = "Replace",
+			},
+		},
+	},
+}
