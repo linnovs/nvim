8a8880d100757498177aba42a5a0c759604a2a0e
Linnovs Liu <no-reply@linnovs.com>
2026-04-05 09:08:28 +0800

    style(snacks): remove keymap vim.schedule

--- plugin/10-snacks.lua
+++ plugin/10-snacks.lua
@@ -26,195 +26,193 @@ vim.schedule(function()
 	})
 end)
 
-vim.schedule(function()
-	local keymaps = {
-		{
-			"<Leader>z",
-			function()
-				Snacks.zen()
-			end,
-			"Toggle Zen mode",
-		},
-		{
-			"<Leader>Z",
-			function()
-				Snacks.zen.zoom()
-			end,
-			"Toggle Zen zoom",
-		},
+local keymaps = {
+    {
+        "<Leader>z",
+        function()
+            Snacks.zen()
+        end,
+        "Toggle Zen mode",
+    },
+    {
+        "<Leader>Z",
+        function()
+            Snacks.zen.zoom()
+        end,
+        "Toggle Zen zoom",
+    },
 
-		{
-			"<Leader>un",
-			function()
-				Snacks.notifier.hide()
-			end,
-			"Dismiss All notifications",
-		},
-		{
-			"<Leader>.",
-			function()
-				Snacks.scratch()
-			end,
-			"Toggle scratch buffer",
-		},
-		{
-			"<Leader>S",
-			function()
-				Snacks.scratch.select()
-			end,
-			"Select scratch buffer",
-		},
-		{
-			"<Leader>n",
-			function()
-				Snacks.notifier.show_history()
-			end,
-			"Notifications history",
-		},
-		{
-			"<Leader>bd",
-			function()
-				Snacks.bufdelete()
-			end,
-			"Delete buffer",
-		},
-		{
-			"<Leader>fbd",
-			function()
-				Snacks.bufdelete({ force = true })
-			end,
-			"Force delete buffer",
-		},
-		{
-			"<Leader>gst",
-			function()
-				Snacks.lazygit()
-			end,
-			"Toggle Lazygit",
-		},
-		{
-			"<Leader>gB",
-			function()
-				Snacks.gitbrowse()
-			end,
-			"Git Browse",
-		},
-		{
-			"<Leader>glf",
-			function()
-				Snacks.lazygit.log_file()
-			end,
-			"Lazygit Current file history",
-		},
-		{
-			"<Leader>gl",
-			function()
-				Snacks.lazygit.log()
-			end,
-			"Lazygit log (CWD)",
-		},
-		{
-			"<Leader>gp",
-			function()
-				Snacks.picker.gh_pr()
-			end,
-			"GitHub Pull Requests (Open)",
-		},
-		{
-			"<Leader>gP",
-			function()
-				Snacks.picker.gh_pr({ state = "all" })
-			end,
-			"GitHub Pull Requests (All)",
-		},
-		{
-			"<Leader>tt",
-			function()
-				Snacks.terminal(nil, { win = { height = 0.3 } })
-			end,
-			"Toggle terminal",
-		},
-		{
-			"]]",
-			function()
-				Snacks.words.jump(vim.v.count1)
-			end,
-			"Next reference",
-		},
-		{
-			"[[",
-			function()
-				Snacks.words.jump(-vim.v.count1)
-			end,
-			"Previous reference",
-		},
-		{
-			"<Leader><Leader>",
-			function()
-				Snacks.picker.files()
-			end,
-			"Find files",
-		},
-		{
-			"<leader>fg",
-			function()
-				Snacks.picker.grep()
-			end,
-			"Grep files",
-		},
-		{
-			"<Leader>fb",
-			function()
-				Snacks.picker.buffers()
-			end,
-			"Find buffers",
-		},
-		{
-			"<Leader>fh",
-			function()
-				Snacks.picker.help()
-			end,
-			"Find help pages",
-		},
-		{
-			"<Leader>fm",
-			function()
-				Snacks.picker.man()
-			end,
-			"Find man pages",
-		},
-		{
-			"<Leader>fe",
-			function()
-				Snacks.picker.explorer()
-			end,
-			"File explorer",
-		},
-		{
-			"<Leader>N",
-			function()
-				Snacks.win({
-					file = vim.api.nvim_get_runtime_file("doc/news.txt", false)[1],
-					width = 0.6,
-					height = 0.6,
-					wo = {
-						winblend = 0,
-						spell = false,
-						wrap = false,
-						signcolumn = "yes",
-						statuscolumn = " ",
-						conceallevel = 3,
-					},
-				})
-			end,
-			"Neovim news",
-		},
-	}
+    {
+        "<Leader>un",
+        function()
+            Snacks.notifier.hide()
+        end,
+        "Dismiss All notifications",
+    },
+    {
+        "<Leader>.",
+        function()
+            Snacks.scratch()
+        end,
+        "Toggle scratch buffer",
+    },
+    {
+        "<Leader>S",
+        function()
+            Snacks.scratch.select()
+        end,
+        "Select scratch buffer",
+    },
+    {
+        "<Leader>n",
+        function()
+            Snacks.notifier.show_history()
+        end,
+        "Notifications history",
+    },
+    {
+        "<Leader>bd",
+        function()
+            Snacks.bufdelete()
+        end,
+        "Delete buffer",
+    },
+    {
+        "<Leader>fbd",
+        function()
+            Snacks.bufdelete({ force = true })
+        end,
+        "Force delete buffer",
+    },
+    {
+        "<Leader>gst",
+        function()
+            Snacks.lazygit()
+        end,
+        "Toggle Lazygit",
+    },
+    {
+        "<Leader>gB",
+        function()
+            Snacks.gitbrowse()
+        end,
+        "Git Browse",
+    },
+    {
+        "<Leader>glf",
+        function()
+            Snacks.lazygit.log_file()
+        end,
+        "Lazygit Current file history",
+    },
+    {
+        "<Leader>gl",
+        function()
+            Snacks.lazygit.log()
+        end,
+        "Lazygit log (CWD)",
+    },
+    {
+        "<Leader>gp",
+        function()
+            Snacks.picker.gh_pr()
+        end,
+        "GitHub Pull Requests (Open)",
+    },
+    {
+        "<Leader>gP",
+        function()
+            Snacks.picker.gh_pr({ state = "all" })
+        end,
+        "GitHub Pull Requests (All)",
+    },
+    {
+        "<Leader>tt",
+        function()
+            Snacks.terminal(nil, { win = { height = 0.3 } })
+        end,
+        "Toggle terminal",
+    },
+    {
+        "]]",
+        function()
+            Snacks.words.jump(vim.v.count1)
+        end,
+        "Next reference",
+    },
+    {
+        "[[",
+        function()
+            Snacks.words.jump(-vim.v.count1)
+        end,
+        "Previous reference",
+    },
+    {
+        "<Leader><Leader>",
+        function()
+            Snacks.picker.files()
+        end,
+        "Find files",
+    },
+    {
+        "<leader>fg",
+        function()
+            Snacks.picker.grep()
+        end,
+        "Grep files",
+    },
+    {
+        "<Leader>fb",
+        function()
+            Snacks.picker.buffers()
+        end,
+        "Find buffers",
+    },
+    {
+        "<Leader>fh",
+        function()
+            Snacks.picker.help()
+        end,
+        "Find help pages",
+    },
+    {
+        "<Leader>fm",
+        function()
+            Snacks.picker.man()
+        end,
+        "Find man pages",
+    },
+    {
+        "<Leader>fe",
+        function()
+            Snacks.picker.explorer()
+        end,
+        "File explorer",
+    },
+    {
+        "<Leader>N",
+        function()
+            Snacks.win({
+                file = vim.api.nvim_get_runtime_file("doc/news.txt", false)[1],
+                width = 0.6,
+                height = 0.6,
+                wo = {
+                    winblend = 0,
+                    spell = false,
+                    wrap = false,
+                    signcolumn = "yes",
+                    statuscolumn = " ",
+                    conceallevel = 3,
+                },
+            })
+        end,
+        "Neovim news",
+    },
+}
 
-	for _, km in ipairs(keymaps) do
-		require("kuuga.lib.keymap")("n", km[1], km[2], km[3])
-	end
-end)
+for _, km in ipairs(keymaps) do
+    require("kuuga.lib.keymap")("n", km[1], km[2], km[3])
+end
 
 require("snacks").setup({
 	bigfile = { enabled = true },
