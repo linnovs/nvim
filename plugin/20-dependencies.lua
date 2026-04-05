73008a3d2e3be63654b97cc2d0d82642bad410d7
Linnovs Liu <no-reply@linnovs.com>
2026-04-06 01:28:37 +0800

    chore(plugin): rename dependencies

--- plugin/20-dependencies.lua
+++ plugin/20-dependencies.lua
@@ -1,4 +1,7 @@
 vim.pack.add({
 	"https://github.com/brenoprata10/nvim-highlight-colors",
 	"https://github.com/nvim-tree/nvim-web-devicons",
+	"https://github.com/b0o/schemastore.nvim",
 })
+
+require("nvim-highlight-colors").setup()
