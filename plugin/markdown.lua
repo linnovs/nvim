cb1ff77cd02cd199ebac9d85b2cb3e962bb8fde0
Linnovs Liu <no-reply@linnovs.com>
2026-04-07 00:08:41 +0800

    feat(plugin): add markview

---
+++ plugin/markdown.lua
@@ -0,0 +1,19 @@
+vim.pack.add({ "https://github.com/OXY2DEV/markview.nvim" })
+local presets = require("markview.presets")
+
+local opts = {
+	preview = {
+		filetypes = { "markdown" },
+		icon_provider = "mini",
+		hybrid_modes = { "n" },
+		linewise_hybrid_mode = true,
+	},
+	markdown = {
+		headings = presets.headings.glow,
+		horizontal_rules = presets.horizontal_rules.thin,
+		tables = presets.tables.rounded,
+	},
+}
+
+require("markview").setup(opts)
+require("markview.extras.editor").setup()
