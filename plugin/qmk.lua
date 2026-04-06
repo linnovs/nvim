7a71d7d359cf4e9664d4eed153f198249b194f10
Linnovs Liu <no-reply@linnovs.com>
2026-04-07 00:08:18 +0800

    feat(plugin): add qmk

---
+++ plugin/qmk.lua
@@ -0,0 +1,18 @@
+vim.api.nvim_create_autocmd("FileType", {
+	pattern = "c",
+	callback = function()
+		vim.pack.add({ "https://github.com/codethread/qmk.nvim" })
+		require("qmk").setup({
+			name = "LAYOUT",
+			comment_preview = {},
+			layout = {
+				"x x x x x x x _ _ _ _ _ x x x x x x x",
+				"x x x x x x x _ _ _ _ _ x x x x x x x",
+				"x x x x x x x _ _ _ _ _ x x x x x x x",
+				"x x x x x x _ _ _ _ _ _ _ x x x x x x",
+				"x x x x x _ x^x _ _ _ x^x _ x x x x x",
+				"_ _ _ _ _ _ x x x _ x x x _ _ _ _ _ _",
+			},
+		})
+	end,
+})
