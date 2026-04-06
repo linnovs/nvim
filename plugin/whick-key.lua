4dc060c6b7b3cd606a0411ef64d7853e4e4ea736
Linnovs Liu <no-reply@linnovs.com>
2026-04-07 00:29:34 +0800

    feat(plugin): add which-key

---
+++ plugin/whick-key.lua
@@ -0,0 +1,29 @@
+vim.schedule(function()
+	vim.pack.add({ "https://github.com/folke/which-key.nvim" })
+	require("kuuga.lib.keymap")(
+		"n",
+		"<Leader>?",
+		function() require("which-key").show({ global = false }) end,
+		"Buffer local keymaps (which-key)"
+	)
+
+	require("which-key").setup({
+		preset = "helix",
+		spec = {
+			{ "g", group = "goto" },
+			{ "]", group = "next" },
+			{ "[", group = "prev" },
+			{ "<leader>c", group = "code" },
+			{ "<leader>d", group = "debug" },
+			{ "<leader>f", group = "find" },
+			{ "<leader>g", group = "git" },
+			{ "<leader>gh", group = "hunks" },
+			{ "<leader>h", group = "harpoon" },
+			{ "<leader>w", group = "windows" },
+			{ "<leader>x", group = "diagnostics/quickfix" },
+			{ "<leader>o", group = "obisdian/explorer" },
+			{ "<leader>l", group = "lazy" },
+			{ "<leader>ze", group = "zen" },
+		},
+	})
+end)
