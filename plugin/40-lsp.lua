3b6e829565783cf12d374f257bce1f9d362b876b
Linnovs Liu <no-reply@linnovs.com>
2026-04-07 22:25:56 +0800

    feat(lsp): enable copilot for all ft

--- plugin/40-lsp.lua
+++ plugin/40-lsp.lua
@@ -1,3 +1,3 @@
 vim.pack.add({ "https://github.com/neovim/nvim-lspconfig" })
-require("kuuga.lib.mason").install({ "harper-ls", "typos-lsp" })
-vim.lsp.enable({ "harper_ls", "typos_lsp" })
+require("kuuga.lib.mason").install({ "harper-ls", "typos-lsp", "copilot-language-server" })
+vim.lsp.enable({ "harper_ls", "typos_lsp", "copilot" })
