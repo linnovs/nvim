b4df7a7d5521c2fba615ae2985d49c9beed11cda
Linnovs Liu <no-reply@linnovs.com>
2026-04-07 08:10:02 +0800

    feat(plugin): buffer navigation (bafa.nvim)

---
+++ plugin/buffer-nav.lua
@@ -0,0 +1,26 @@
+vim.schedule(function()
+	vim.pack.add({ "https://github.com/mistweaverco/bafa.nvim" })
+	require("kuuga.lib.keymap")(
+		"n",
+		"<Leader>s",
+		function()
+			require("bafa.ui").toggle({
+				with_jump_labels = true,
+			})
+		end,
+		"Buffer navigation with bafa.nvim"
+	)
+	require("bafa").setup({
+		ui = {
+			jump_labels = {
+					-- stylua: ignore start
+					keys = {
+						"a", "r", "s", "t", "n", "e", "i", "o", -- home row
+						"q", "w", "f", "p", "l", "u", "y", ";", -- top row
+						"z", "x", "c", "k", "h", ",", ".",      -- bottom row
+					},
+				-- stylua: ignore end
+			},
+		},
+	})
+end)
