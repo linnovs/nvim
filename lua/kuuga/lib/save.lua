f7b7d45fffd3ab84eb04d02b8bbeeaaf343700cc
Linnovs Liu <no-reply@linnovs.com>
2026-04-10 00:55:18 +0800

    chore(save): remove silent modifier from write command

--- lua/kuuga/lib/save.lua
+++ lua/kuuga/lib/save.lua
@@ -2,7 +2,7 @@ return function()
 	local bufnr = vim.api.nvim_get_current_buf()
 
 	if vim.api.nvim_get_option_value("modified", { buf = bufnr }) then
-		vim.cmd.write({ mods = { silent = true } })
+		vim.cmd.write()
 	else
 		vim.notify("No changes to save", vim.log.levels.INFO)
 	end
