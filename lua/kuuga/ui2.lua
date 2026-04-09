32ae21aee7fe328b483acf2e82ab7cf01d711aa1
Linnovs Liu <no-reply@linnovs.com>
2026-04-09 10:43:00 +0800

    feat(ui2): update message targets

--- lua/kuuga/ui2.lua
+++ lua/kuuga/ui2.lua
@@ -1,11 +1,36 @@
 require("vim._core.ui2").enable({
 	enable = true, -- Whether to enable or disable the UI.
 	msg = { -- Options related to the message module.
-		---@type 'cmd'|'msg' Default message target, either in the
-		---cmdline or in a separate ephemeral message window.
 		---@type string|table<string, 'cmd'|'msg'|'pager'> Default message target
 		---or table mapping |ui-messages| kinds and triggers to a target.
-		targets = "msg",
+		targets = {
+			[""] = "msg",
+			empty = "cmd",
+			bufwrite = "msg",
+			confirm = "cmd",
+			emsg = "pager",
+			echo = "msg",
+			echomsg = "msg",
+			echoerr = "pager",
+			completion = "cmd",
+			list_cmd = "pager",
+			lua_error = "pager",
+			lua_print = "msg",
+			progress = "msg",
+			rpc_error = "pager",
+			quickfix = "msg",
+			search_cmd = "cmd",
+			search_count = "cmd",
+			shell_cmd = "pager",
+			shell_err = "pager",
+			shell_out = "pager",
+			shell_ret = "pager",
+			undo = "msg",
+			verbose = "pager",
+			wildlist = "cmd",
+			wmsg = "msg",
+			typed_cmd = "cmd",
+		},
 		cmd = { -- Options related to messages in the cmdline window.
 			height = 0.5, -- Maximum height while expanded for messages beyond 'cmdheight'.
 		},
