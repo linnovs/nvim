66cc7c8a6d16fa7f71910c68bfa99f5dff35b651
Linnovs Liu <no-reply@linnovs.com>
2026-04-12 05:34:14 +0800

    feat(settings): update cursor line settings

--- lua/kuuga/settings.lua
+++ lua/kuuga/settings.lua
@@ -5,7 +5,6 @@ vim.g.maplocalleader = " "
 -- Set up options
 vim.opt.colorcolumn = "+1" -- columns to highlight
 vim.opt.confirm = true -- Confirm to save changes before exiting modified buffer
-vim.opt.cursorline = true -- highlight the current line
 vim.opt.diffopt:append({ algorithm = "histogram", linematch = 60 }) -- options for using diff mode
 vim.opt.exrc = true -- allow project-local .nvimrc files
 vim.opt.inccommand = "split" -- preview substitutions live
@@ -16,6 +15,10 @@ vim.opt.showmode = false -- hide the mode since it's shown in the statusline
 vim.opt.signcolumn = "yes" -- keep signcolumn on by default
 vim.opt.textwidth = 120 -- set maximum width of text that is being inserted
 vim.opt.undofile = true -- save undo history to an undo file
+
+-- cursor lines settings
+vim.opt.cursorline = true
+vim.opt.cursorlineopt = "screenline,number"
 
 -- line number
 vim.opt.number = true
