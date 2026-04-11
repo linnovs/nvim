e358290a99e78a25f15da44f24391b86c87ce53f
Linnovs Liu <no-reply@linnovs.com>
2026-04-11 11:16:21 +0800

    feat(colorscheme): customize border color for tokyonight

--- plugin/00-colorschemme.lua
+++ plugin/00-colorschemme.lua
@@ -1,4 +1,8 @@
 local gh = require("kuuga.helper.gh")
 vim.pack.add({ gh("folke/tokyonight.nvim") })
-require("tokyonight").setup({ style = "storm", transparent = true })
+require("tokyonight").setup({
+	style = "storm",
+	transparent = true,
+	on_colors = function(colors) colors.border = "#0a0b10" end,
+})
 vim.cmd.colorscheme("tokyonight")
