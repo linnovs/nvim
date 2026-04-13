8e9174e4eb8a07b06ed63b98eb419faa8e0b11ed
Linnovs Liu <no-reply@linnovs.com>
2026-04-13 13:36:03 +0800

    fix(plugin): remove plenary.nvim dependency

--- plugin/20-dependencies.lua
+++ plugin/20-dependencies.lua
@@ -1,6 +1,5 @@
 local gh = require("kuuga.helper.gh")
 vim.pack.add({
-	gh("nvim-lua/plenary.nvim"),
 	gh("brenoprata10/nvim-highlight-colors"),
 	gh("nvim-tree/nvim-web-devicons"),
 	gh("b0o/schemastore.nvim"),
