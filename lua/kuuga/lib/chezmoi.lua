b28e131b9528e492685a2b9c084081f8315bd989
Linnovs Liu <no-reply@linnovs.com>
2026-04-14 15:00:15 +0800

    feat(chezmoi): remove uv dependency and use vim.system for applying changes

--- lua/kuuga/lib/chezmoi.lua
+++ lua/kuuga/lib/chezmoi.lua
@@ -1,5 +1,4 @@
 local homedir = os.getenv("HOME")
-local uv = vim.uv
 
 local function notify(msg, level)
 	vim.schedule(function() vim.notify(msg, level, { title = "Chezmoi", timeout = 2000 }) end)
@@ -10,43 +9,25 @@ return function(args)
 	local file = args.file:match("^.+/(.+)$") or args.file
 	local is_run = string.starts(file, "run_")
 	local is_chezmoi = string.starts(file, ".chezmoi")
+	local is_commitmsg = string.starts(file, "COMMIT_EDITMSG")
 
-	if is_run or is_chezmoi then return end
+	if is_run or is_chezmoi or is_commitmsg then return end
 
-	local stdin = uv.new_pipe()
-	local stdout = uv.new_pipe()
+	local out = vim.system(
+		{ "chezmoi", "apply", "--no-pager", "--no-tty", "--refresh-externals=never", "--source-path", args.file },
+		{ cwd = homedir .. "/.local/share/chezmoi", stdin = "diff\n" }
+	):wait()
 
-	if stdin == nil or stdout == nil then
-		notify("Failed to create pipes for child process", vim.log.levels.ERROR)
-		return
-	end
-
-	uv.spawn("chezmoi", {
-		args = { "apply", "--no-pager", "--no-tty", "--refresh-externals=never", "--source-path", args.file },
-		stdio = { stdin, stdout, nil },
-		cwd = homedir .. "/.local/share/chezmoi",
-	}, function(code)
-		if code ~= 0 then
-			if stdout == nil then
-				notify("Apply failed with code: " .. code, vim.log.levels.ERROR)
-				return
-			end
-			uv.read_start(stdout, function(err, data)
-				if err then
-					notify("Error reading stdout: " .. err, vim.log.levels.ERROR)
-					return
-				end
-				if data then
-					notify("Apply failed: " .. data, vim.log.levels.ERROR)
-				else
-					notify("Apply failed with code: " .. code, vim.log.levels.ERROR)
-				end
-			end)
-		else
-			notify("Chezmoi apply successful", vim.log.levels.INFO)
+	if out.code == 0 then
+		notify("Chezmoi apply successful", vim.log.levels.INFO)
+	else
+		if out.stdout and out.stdout ~= "" then
+			notify("Chezmoi apply failed: " .. out.stdout, vim.log.levels.ERROR)
+			return
+		elseif out.stderr and out.stderr ~= "" then
+			notify("Chezmoi apply failed: " .. out.stderr, vim.log.levels.ERROR)
+			return
 		end
-	end)
-
-	uv.write(stdin, "diff\n")
-	uv.shutdown(stdin)
+		notify("Chezmoi apply failed", vim.log.levels.ERROR)
+	end
 end
