e83cc149fa5ad56ed212bcef663d03adb54b29cc
Linnovs Liu <no-reply@linnovs.com>
2026-04-14 09:34:27 +0800

    feat(statusline): change macro recording icon

--- lua/kuuga/statusline/components/macro_recording.lua
+++ lua/kuuga/statusline/components/macro_recording.lua
@@ -4,7 +4,7 @@ return function()
 
 	return table.concat({
 		"%#StatuslineMacroRecordingIcon#",
-		"󰰟 ",
+		"󰑋 ",
 		"%#StatuslineMacroRecording#",
 		"Recording ",
 		"%#StatuslineMacroRecordingReg#",
