9d5e89ffbf9766ab1c520e08e85ee66152985e83
Linnovs Liu <no-reply@linnovs.com>
2026-04-14 11:07:18 +0800

    feat(diagnostics): update diagnostic icons

--- lua/kuuga/statusline/components/diagnostics.lua
+++ lua/kuuga/statusline/components/diagnostics.lua
@@ -1,3 +1,5 @@
+local icons = require("kuuga.lib.icons").diagnostics
+
 local function get_diagnostic_counts()
 	return vim.diagnostic.count(nil, { severity = { min = vim.diagnostic.severity.HINT } })
 end
@@ -9,18 +11,22 @@ return function()
 	local results = {
 		errors = diagnostics[vim.diagnostic.severity.ERROR] or 0,
 		warnings = diagnostics[vim.diagnostic.severity.WARN] or 0,
-		info = diagnostics[vim.diagnostic.severity.INFO] or 0,
+		infos = diagnostics[vim.diagnostic.severity.INFO] or 0,
 		hints = diagnostics[vim.diagnostic.severity.HINT] or 0,
 	}
-	local total = results.errors + results.warnings + results.info + results.hints
+	local total = results.errors + results.warnings + results.infos + results.hints
 
 	if vim.bo[bufnr].modifiable and total > 0 then
 		local msgs = {}
 
-		if results.errors > 0 then table.insert(msgs, "%#DiagnosticError# %*" .. results.errors) end
-		if results.warnings > 0 then table.insert(msgs, "%#DiagnosticWarn# %*" .. results.warnings) end
-		if results.info > 0 then table.insert(msgs, "%#DiagnosticInfo# %*" .. results.info) end
-		if results.hints > 0 then table.insert(msgs, "%#DiagnosticHint# %*" .. results.hints) end
+		if results.errors > 0 then
+			table.insert(msgs, "%#DiagnosticError#" .. icons.error .. " %*" .. results.errors)
+		end
+		if results.warnings > 0 then
+			table.insert(msgs, "%#DiagnosticWarn#" .. icons.warn .. " %*" .. results.warnings)
+		end
+		if results.infos > 0 then table.insert(msgs, "%#DiagnosticInfo#" .. icons.info .. " %*" .. results.infos) end
+		if results.hints > 0 then table.insert(msgs, "%#DiagnosticHint#" .. icons.hint .. " %*" .. results.hints) end
 
 		return table.concat(msgs, " ")
 	end
