b78224d774376883a9b3a86ffdd41807061dd9f2
Linnovs Liu <no-reply@linnovs.com>
2026-04-14 08:05:25 +0800

    feat(statusline): components use alias for better type safety

--- lua/kuuga/statusline/components/init.lua
+++ lua/kuuga/statusline/components/init.lua
@@ -1,36 +1,33 @@
 local M = {}
 local sep = require("kuuga.statusline.components.sep")
 
----@enum ComponentNames
-local component_names = {
-	mode = "mode",
-	filepath = "filepath",
-	file = "file",
-	git = "git",
-	macro_recording = "macro_recording",
-	search_count = "search_count",
-	buffer_info = "buffer_info",
-	line_info = "line_info",
-	filetype = "filetype",
-	scrollbar = "scrollbar",
-	diagnostics = "diagnostics",
-	lsp = "lsp",
-}
-
+---@alias ComponentNames
+---| "mode"
+---| "filepath"
+---| "file"
+---| "git"
+---| "macro_recording"
+---| "search_count"
+---| "buffer_info"
+---| "line_info"
+---| "filetype"
+---| "scrollbar"
+---| "diagnostics"
+---| "lsp"
 ---@type table<ComponentNames, fun(): string>
 local components = {
-	[component_names.mode] = require("kuuga.statusline.components.mode"),
-	[component_names.filepath] = require("kuuga.statusline.components.filepath"),
-	[component_names.file] = require("kuuga.statusline.components.file"),
-	[component_names.git] = require("kuuga.statusline.components.git"),
-	[component_names.macro_recording] = require("kuuga.statusline.components.macro_recording"),
-	[component_names.search_count] = require("kuuga.statusline.components.search_count"),
-	[component_names.buffer_info] = require("kuuga.statusline.components.buffer_info"),
-	[component_names.line_info] = require("kuuga.statusline.components.line_info"),
-	[component_names.filetype] = require("kuuga.statusline.components.filetype"),
-	[component_names.scrollbar] = require("kuuga.statusline.components.scrollbar"),
-	[component_names.diagnostics] = require("kuuga.statusline.components.diagnostics"),
-	[component_names.lsp] = require("kuuga.statusline.components.lsp"),
+	mode = require("kuuga.statusline.components.mode"),
+	filepath = require("kuuga.statusline.components.filepath"),
+	file = require("kuuga.statusline.components.file"),
+	git = require("kuuga.statusline.components.git"),
+	macro_recording = require("kuuga.statusline.components.macro_recording"),
+	search_count = require("kuuga.statusline.components.search_count"),
+	buffer_info = require("kuuga.statusline.components.buffer_info"),
+	line_info = require("kuuga.statusline.components.line_info"),
+	filetype = require("kuuga.statusline.components.filetype"),
+	scrollbar = require("kuuga.statusline.components.scrollbar"),
+	diagnostics = require("kuuga.statusline.components.diagnostics"),
+	lsp = require("kuuga.statusline.components.lsp"),
 }
 
 ---@param name ComponentNames
