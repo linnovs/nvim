05d89be3bcbe2a459cffb281ee7fe47dc09133e5
Linnovs Liu <no-reply@linnovs.com>
2026-04-15 09:29:58 +0800

    feat(statusline): remove selected lines/chars in line_info component

--- lua/kuuga/statusline/components/line_info.lua
+++ lua/kuuga/statusline/components/line_info.lua
@@ -1,35 +1,5 @@
-local function selected_lines()
-	local lines = math.abs(vim.fn.line("v") - vim.fn.line(".")) + 1
-
-	if lines <= 1 then return "" end
-
-	return table.concat({
-		"%#StatusLineLineInfoSLineIcon#",
-		" ",
-		"%#StatusLineLineInfoLine#",
-		lines,
-		" ",
-	})
-end
-
-local function selected_chars()
-	local words = vim.fn.wordcount()
-
-	if not words.visual_chars then return "" end
-
-	return table.concat({
-		"%#StatusLineLineInfoSCharIcon#",
-		"󰘎 ",
-		"%#StatusLineLineInfoLine#",
-		words.visual_chars,
-		" ",
-	})
-end
-
 return function()
 	return table.concat({
-		selected_lines(),
-		selected_chars(),
 		"%#StatusLineLineInfoIcon#",
 		" ",
 		"%#StatusLineLineInfoLine#",
