2f0561052677913176274bcc0d8e575658239bad
Linnovs Liu <no-reply@linnovs.com>
2026-04-15 13:32:15 +0800

    feat(autocmd): add descriptions to autocmds

--- lua/kuuga/autocmds.lua
+++ lua/kuuga/autocmds.lua
@@ -2,11 +2,21 @@ local autocmd = vim.api.nvim_create_autocmd
 local augroup = vim.api.nvim_create_augroup
 local homedir = os.getenv("HOME")
 
-autocmd({ "FocusGained", "TermClose", "TermLeave" }, { command = "checktime" })
-autocmd("VimResized", { command = "tabdo wincmd =" })
-autocmd("TextYankPost", { callback = function() vim.hl.on_yank() end })
-autocmd("BufWritePost", { pattern = homedir .. "/.local/share/chezmoi/*", callback = require("kuuga.lib.chezmoi") })
-autocmd("FileType", { pattern = "gitcommit", command = "startinsert" })
+autocmd(
+	{ "FocusGained", "TermClose", "TermLeave" },
+	{ command = "checktime", desc = "Check for file changes when focus is gained or terminal is closed/left" }
+)
+autocmd("VimResized", { command = "tabdo wincmd =", desc = "Resize windows when Vim is resized" })
+autocmd("TextYankPost", { callback = function() vim.hl.on_yank() end, desc = "Highlight yanked text" })
+autocmd("BufWritePost", {
+	pattern = homedir .. "/.local/share/chezmoi/*",
+	callback = require("kuuga.lib.chezmoi"),
+	desc = "Run chezmoi apply on buffer write",
+})
+autocmd(
+	"FileType",
+	{ pattern = "gitcommit", command = "startinsert", desc = "Auto-enter insert mode for git commit messages" }
+)
 autocmd({
 	"WinEnter",
 	"BufEnter",
