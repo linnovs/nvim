e08335d015e85f34ae1b0792b98a6fb98fe10530
Linnovs Liu <no-reply@linnovs.com>
2026-04-16 10:51:38 +0800

    feat(lua): update lazydev.nvim and remove custom lua_ls config

--- after/lsp/lua_ls.lua
+++ after/lsp/lua_ls.lua
@@ -1,34 +1,4 @@
-local library = vim.tbl_filter(
-	function(d) return d:match(vim.fn.stdpath("data") .. "/site/pack/core/opt") or d:match(vim.env.VIMRUNTIME) end,
-	vim.api.nvim_get_runtime_file("", true)
-)
-
 return {
-	on_init = function(client)
-		if client.workspace_folders then
-			local path = client.workspace_folders[1].name
-			if
-				path ~= vim.fn.stdpath("config")
-				and (vim.uv.fs_stat(path .. "/.luarc.json") or vim.uv.fs_stat(path .. "/.luarc.jsonc"))
-			then
-				return
-			end
-		end
-
-		client.config.settings.Lua = vim.tbl_deep_extend("force", client.config.settings.Lua, {
-			runtime = {
-				version = "LuaJIT",
-				path = {
-					"lua/?.lua",
-					"lua/?/init.lua",
-					"${3rd}/luv/library",
-				},
-			},
-			workspace = {
-				library = library,
-			},
-		})
-	end,
 	settings = {
 		Lua = {
 			hint = {
--- ftplugin/lua.lua
+++ ftplugin/lua.lua
@@ -1,5 +1,9 @@
 vim.opt.expandtab = false
 
+local gh = require("kuuga.helper.gh")
+vim.pack.add({ gh("folke/lazydev.nvim") })
+require("lazydev").setup()
+
 require("kuuga.lib.treesitter").start()
 require("kuuga.lib.mason").install({ "stylua", "lua-language-server" })
 require("kuuga.lib.formatter").setup({ "stylua" })
