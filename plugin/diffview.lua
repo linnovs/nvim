76bb2392847bf6fd9da521daae9687f371659f05
Linnovs Liu <no-reply@linnovs.com>
2026-04-17 07:19:41 +0800

    feat(diffview): add keymap for opening diffview

--- plugin/diffview.lua
+++ plugin/diffview.lua
@@ -1,7 +1,10 @@
 local gh = require("kuuga.helper.gh")
 local usercmd = require("kuuga.lib.pack-usercmd").user_command
+local keymap = require("kuuga.lib.keymap")
 
 local function packadd() vim.pack.add({ gh("dlyongemallo/diffview.nvim") }) end
 
 usercmd("DiffviewOpen", packadd)
 usercmd("DiffviewFileHistory", packadd)
+
+keymap("n", "<Leader>dvo", "<Cmd>DiffviewOpen<CR>", "Open Diffview")
