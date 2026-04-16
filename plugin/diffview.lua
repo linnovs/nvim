46ad64980f71d2d771ba8f02138426a32e5742ba
Linnovs Liu <no-reply@linnovs.com>
2026-04-17 07:27:35 +0800

    feat(diffview): setup after packadd

--- plugin/diffview.lua
+++ plugin/diffview.lua
@@ -2,7 +2,19 @@ local gh = require("kuuga.helper.gh")
 local usercmd = require("kuuga.lib.pack-usercmd").user_command
 local keymap = require("kuuga.lib.keymap")
 
-local function packadd() vim.pack.add({ gh("dlyongemallo/diffview.nvim") }) end
+local need_setup = true
+local function setup()
+	if need_setup then
+		require("diffview").setup({
+			enhanced_diff_hl = true,
+		})
+		need_setup = false
+	end
+end
+local function packadd()
+	vim.pack.add({ gh("dlyongemallo/diffview.nvim") })
+	setup()
+end
 
 usercmd("DiffviewOpen", packadd)
 usercmd("DiffviewFileHistory", packadd)
