31444478a0f82a781c39a77acd5fa023aab26754
Linnovs Liu <no-reply@linnovs.com>
2026-04-17 16:59:07 +0800

    feat(linting): remove autocmd, run vim.pack directly

--- plugin/linting.lua
+++ plugin/linting.lua
@@ -1,11 +1,9 @@
 local gh = require("kuuga.helper.gh")
-vim.api.nvim_create_autocmd("BufReadPost", {
-	callback = function()
-		vim.pack.add({ gh("mfussenegger/nvim-lint") })
-		require("lint").linters_by_ft = { ["*"] = {} } -- reset
-		vim.api.nvim_create_autocmd({ "BufWritePost" }, {
-			group = vim.api.nvim_create_augroup("KuugaLint", { clear = true }),
-			callback = function() require("lint").try_lint() end,
-		})
-	end,
+
+vim.pack.add({ gh("mfussenegger/nvim-lint") })
+
+require("lint").linters_by_ft = { ["*"] = {} } -- reset
+vim.api.nvim_create_autocmd("BufWritePost", {
+	pattern = "*",
+	callback = function() require("lint").try_lint() end,
 })
