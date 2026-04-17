90b8b9135810635d60ec060c28480a659a98f7ba
Linnovs Liu <no-reply@linnovs.com>
2026-04-17 16:59:50 +0800

    feat(formatter): remove autocmd, and trim_whitespace formatter

--- plugin/formatter.lua
+++ plugin/formatter.lua
@@ -1,25 +1,19 @@
 local gh = require("kuuga.helper.gh")
-vim.api.nvim_create_autocmd("BufReadPre", {
-	callback = function()
-		vim.pack.add({ { src = gh("stevearc/conform.nvim"), version = vim.version.range("*") } })
-		vim.opt.formatexpr = 'v:lua.require("conform").formatexpr()'
 
-		require("kuuga.lib.keymap")(
-			"n",
-			"<Leader>f",
-			function() require("conform").format({ lsp_format = "fallback", async = true }) end,
-			"Format buffer"
-		)
+vim.pack.add({ { src = gh("stevearc/conform.nvim"), version = vim.version.range("*") } })
+vim.opt.formatexpr = 'v:lua.require("conform").formatexpr()'
 
-		require("conform").setup({
-			format_on_save = function(bufnr)
-				if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then return end
-				return { timeout_ms = 500, lsp_format = "fallback" }
-			end,
-			formatters_by_ft = {
-				["*"] = { "trim_whitespace" },
-			},
-			notify_on_error = true,
-		})
+require("kuuga.lib.keymap")(
+	"n",
+	"<Leader>f",
+	function() require("conform").format({ lsp_format = "fallback", async = true }) end,
+	"Format buffer"
+)
+
+require("conform").setup({
+	format_on_save = function(bufnr)
+		if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then return end
+		return { timeout_ms = 500, lsp_format = "fallback" }
 	end,
+	notify_on_error = true,
 })
