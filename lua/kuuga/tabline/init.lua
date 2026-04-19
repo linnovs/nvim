2a0bc36e5c3ab319573c0e88c5aa189bb7fb86da
Linnovs Liu <no-reply@linnovs.com>
2026-04-20 01:04:52 +0800

    refactor(tabline): change tabline render context

--- lua/kuuga/tabline/init.lua
+++ lua/kuuga/tabline/init.lua
@@ -2,4 +2,4 @@ require("kuuga.tabline.highlights")
 require("kuuga.tabline.tabline")
 
 TabLine.refresh()
-vim.opt.tabline = "%!v:lua.TabLine.render()"
+vim.opt.tabline = "%{%v:lua.TabLine.render()%}"
