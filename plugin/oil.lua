852b04779eeb0b464db47756f19679aa0512d0ea
Linnovs Liu <no-reply@linnovs.com>
2026-04-20 11:45:42 +0800

    feat(oil): enable watch for changes and autosave unmodified after lsp rename

--- plugin/oil.lua
+++ plugin/oil.lua
@@ -4,6 +4,8 @@ vim.api.nvim_create_autocmd("VimEnter", {
 		require("kuuga.lib.keymap")("n", "<Leader>of", "<Cmd>Oil<CR>", "Open oil (File explorer)")
 		require("oil").setup({
 			default_file_explorer = true,
+			watch_for_changes = true,
+			lsp_file_methods = { autosave_changes = "unmodified" },
 			keymaps = {
 				["<Leader>wv"] = "actions.select_vsplit",
 				["<C-s>"] = false,
