f75b5d53abc51d8344d21b17ef28413510a7a295
Linnovs Liu <no-reply@linnovs.com>
2026-04-22 18:30:10 +0800

    feat(oil): disable lsp and add detail view toggle

--- plugin/oil.lua
+++ plugin/oil.lua
@@ -1,3 +1,4 @@
+local detail = false
 vim.api.nvim_create_autocmd("VimEnter", {
 	callback = function()
 		vim.pack.add({ { src = "https://github.com/stevearc/oil.nvim", version = vim.version.range("*") } })
@@ -8,6 +9,17 @@ vim.api.nvim_create_autocmd("VimEnter", {
 			lsp_file_methods = { autosave_changes = "unmodified" },
 			keymaps = {
 				["<Leader>wv"] = "actions.select_vsplit",
+				["gd"] = {
+					desc = "Toggle detail view",
+					callback = function()
+						detail = not detail
+						if detail then
+							require("oil").set_columns({ "icon", "permissions", "size", "mtime" })
+						else
+							require("oil").set_columns({ "icon" })
+						end
+					end,
+				},
 				["<C-s>"] = false,
 			},
 		})
